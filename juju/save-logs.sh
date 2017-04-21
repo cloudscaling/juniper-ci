de87c25a97796d87ab906e04e7094e1112db2133
Andrey Pavlov <andrey.mp@gmail.com>
2017-04-21 15:11:16 +0300

    fix path to common functions

--- juju/save-logs.sh
+++ juju/save-logs.sh
@@ -2,7 +2,7 @@
 
 my_file="$(readlink -e "$0")"
 my_dir="$(dirname $my_file)"
-source $my_dir/functions
+source $my_dir/common/functions
 
 echo "--------------------------------------------------- Save LOGS ---"
 
