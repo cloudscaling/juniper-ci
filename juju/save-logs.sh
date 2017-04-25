e1af73d3a7c8033c2b9ce8c0fc0cd70e8afbb9d2
Andrey Pavlov <andrey.mp@gmail.com>
2017-04-25 19:02:34 +0300

    save statuses log for units

--- juju/save-logs.sh
+++ juju/save-logs.sh
@@ -12,6 +12,15 @@ log_dir=$WORKSPACE/logs
 juju-status > $log_dir/juju_status.log
 juju-status-tabular > $log_dir/juju_status_tabular.log
 
+truncate -s 0 $log_dir/juju_unit_statuses.log
+for unit in `juju status --format oneline | awk '{print $2}' | sed 's/://g'` ; do
+  if [ -z "$unit" || "$unit" =~ "ubuntu/" || "$unit" =~ "ntp/" ] ; then
+    continue
+  fi
+  echo "--------------------------------- $unit statuses log" >> $log_dir/juju_unit_statuses.log
+  juju show-status-log $unit >> $log_dir/juju_unit_statuses.log
+done
+
 for mch in $(juju-get-machines) ; do
   juju-ssh $mch "rm -f logs.* ; sudo tar -cf logs.tar /var/log/juju ; gzip logs.tar" 2>/dev/null
   rm -f logs.tar.gz
