df67fbd24f29f98e68f0452805f6769f66d06db7
Andrey Pavlov <andrey.mp@gmail.com>
2017-05-06 13:22:44 +0300

    add contrail-status and vif outputs to logs

--- juju/__save-logs.sh
+++ juju/__save-logs.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
 rm -f logs.*
-sudo tar -cf logs.tar /var/log/juju
+tar -cf logs.tar /var/log/juju
 for ldir in '/etc/apache2' '/etc/apt' '/etc/contrail' '/etc/contrailctl' '/etc/neutron' '/etc/nova' '/var/log/upstart' '/var/log/neutron' '/var/log/nova' '/var/log/contrail' ; do
   if [ -d "$ldir" ] ; then
     tar -rf logs.tar "$ldir"
   fi
 done
+
+if which contrail-status ; then
+  contrail-status &>contrail-status.log
+  tar -rf logs.tar contrail-status.log
+fi
+
+if which vif ; then
+  vif --list &>vif.log
+  tar -rf logs.tar vif.log
+fi
 
 if docker ps | grep -q contrail ; then
   DL='docker-logs'
@@ -16,6 +26,7 @@ if docker ps | grep -q contrail ; then
       ldir="$DL/contrail-$cnt"
       mkdir -p "$ldir"
       docker logs "contrail-$cnt" &>"./$ldir/$cnt.log"
+      docker exec contrail-$cnt contrail-status &>"./$ldir/contrail-status.log"
       docker cp "contrail-$cnt:/var/log/contrail" "./$ldir"
       mv "$ldir/contrail" "$ldir/contrail-logs"
       docker cp "contrail-$cnt:/etc/contrail" "./$ldir"
