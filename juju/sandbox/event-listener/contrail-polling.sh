a56c0b99843d74efa5dfcae45e22eb311c03f507
alexey-mr <alexey.morlang@gmail.com>
2017-05-22 18:42:24 +0300

    fixed fips polling in case of no fips

--- juju/sandbox/event-listener/contrail-polling.sh
+++ juju/sandbox/event-listener/contrail-polling.sh
@@ -11,23 +11,23 @@ export FIP_DISASSOCIATE_SCRIPT=${FIP_DISASSOCIATE_SCRIPT:-"${my_dir}/fip-disasso
 while true; do
     instances=`openstack server list --all-projects`
     fips=`openstack floating ip list --long --noindent -f table`
-    echo ${fips}
     fips_arr=(`echo "${fips}" | awk '/ACTIVE/{print($4","$6)}'`)
-    echo ${fips_arr[@]}
-    for fip in ${fips_arr[@]} ; do
-        floating_ip=`echo ${fip} | cut -d ',' -f 1`
-        fixed_ip=`echo ${fip} | cut -d ',' -f 2`
-        if [[ "${fixed_ip}" != 'None' ]] ; then
-            vm_uuid=`echo "${instances}" | grep "${fixed_ip}" | awk '{print $2}'`
-            ${FIP_ASSOCIATE_SCRIPT} ${vm_uuid} ${floating_ip} || /bin/true
-        else
-            # cleanup rules and vgw for dis-associated fips
-            compute_nodes=`openstack compute service list -c Host --service nova-compute -f value`
-            for n in $compute_nodes ; do
-                SSH_NODE_ADDRESS=${n} ${FIP_DISASSOCIATE_SCRIPT} ${floating_ip} || /bin/true
-            done
-        fi
-    done
+    if [[ -n "${fips_arr[@]}" ]] ; then
+        for fip in ${fips_arr[@]} ; do
+            floating_ip=`echo ${fip} | cut -d ',' -f 1`
+            fixed_ip=`echo ${fip} | cut -d ',' -f 2`
+            if [[ "${fixed_ip}" != 'None' ]] ; then
+                vm_uuid=`echo "${instances}" | grep "${fixed_ip}" | awk '{print $2}'`
+                ${FIP_ASSOCIATE_SCRIPT} ${vm_uuid} ${floating_ip} || /bin/true
+            else
+                # cleanup rules and vgw for dis-associated fips
+                compute_nodes=`openstack compute service list -c Host --service nova-compute -f value`
+                for n in $compute_nodes ; do
+                    SSH_NODE_ADDRESS=${n} ${FIP_DISASSOCIATE_SCRIPT} ${floating_ip} || /bin/true
+                done
+            fi
+        done
+    fi
 
     sleep 2
 done
