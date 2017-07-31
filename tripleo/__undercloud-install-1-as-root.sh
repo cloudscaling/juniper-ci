c0c05d9c5bc5a348187725dca447a01e3aa3404d
alexey-mr <alexey.morlang@gmail.com>
2017-07-31 12:46:21 +0300

    tripleo ci: add liburcu2-0.8.6-21.1.x86_64.rpm into contrail repo for dpdk

--- tripleo/__undercloud-install-1-as-root.sh
+++ tripleo/__undercloud-install-1-as-root.sh
@@ -141,13 +141,28 @@ done
 mkdir -p /var/www/html/contrail
 tar -xvf /opt/contrail/contrail_packages/contrail_rpms.tgz -C /var/www/html/contrail
 
-# hack: centos images don have openstack-utilities packages
-if [[ "$ENVIRONMENT_OS" == 'centos' ]] ; then
-  wget -P /var/www/html/contrail  http://mirror.comnet.uz/centos/7/cloud/x86_64/openstack-newton/common/openstack-utils-2017.1-1.el7.noarch.rpm
-  createrepo --update -v /var/www/html/contrail
-fi
-
 # prepare docker images
 for i in `ls /root/contrail_packages/*.tgz` ; do
   tar -xvf ${i} -C /var/www/html/contrail
 done
+
+update_contrail_repo='no'
+
+# hack: centos images don have openstack-utilities packages
+if [[ "$ENVIRONMENT_OS" == 'centos' ]] ; then
+  curl -o /var/www/html/contrail/openstack-utils-2017.1-1.el7.noarch.rpm http://mirror.comnet.uz/centos/7/cloud/x86_64/openstack-newton/common/openstack-utils-2017.1-1.el7.noarch.rpm
+  update_contrail_repo='yes'
+fi
+
+# TODO: contrail-vrouter-dpdk depends on liburcu2
+#       temprorary add this package into contrail repo.
+#       It should be fixed by addind the package either into contrail distribution
+#       or into RedHat repo or by enabling additional repo.
+if [[ "$DPDK" == 'yes' ]] ; then
+  curl -o /var/www/html/contrail/liburcu2-0.8.6-21.1.x86_64.rpm  ftp://ftp.icm.edu.pl/vol/rzm6/linux-opensuse/repositories/devel:/tools:/lttng/RedHat_RHEL-5/x86_64/liburcu2-0.8.6-21.1.x86_64.rpm
+  update_contrail_repo='yes'
+fi
+
+if [[ "$update_contrail_repo" != 'no' ]] ; then
+  createrepo --update -v /var/www/html/contrail
+fi
