66f1be28397d2a511956b2957ac3f11c3f984867
alexey-mr <alexey.morlang@gmail.com>
2017-08-04 15:11:57 +0300

    tripleo ci: remove USE_ENV and always use venv, use DPDK variable

--- tripleo/check-contrail-proxy.sh
+++ tripleo/check-contrail-proxy.sh
@@ -12,7 +12,6 @@ shift
 ssh_opts=$@
 
 MAX_FAIL=${MAX_FAIL:-30}
-USE_VENV=${USE_VENV:-'true'}
 SSH_CMD=${SSH_CMD:-'ssh'}
 
 cos_dir='/home/stack/check-openstack'
@@ -20,5 +19,5 @@ ssh -T $ssh_opts $ssh_addr "mkdir -p $cos_dir/tripleo"
 scp $ssh_opts $my_dir/check-openstack.sh $ssh_addr:$cos_dir/tripleo/
 scp $ssh_opts -r $my_dir/../common $ssh_addr:$cos_dir/
 ssh -T $ssh_opts $ssh_addr "chown -R stack:stack $cos_dir"
-run_opts="SSH_CMD=$SSH_CMD USE_VENV=$USE_VENV MAX_FAIL=$MAX_FAIL DEBUG=$DEBUG OPENSTACK_VERSION=$OPENSTACK_VERSION USE_DPDK=$DPDK"
+run_opts="SSH_CMD=$SSH_CMD MAX_FAIL=$MAX_FAIL DEBUG=$DEBUG OPENSTACK_VERSION=$OPENSTACK_VERSION DPDK=$DPDK"
 ssh -T $ssh_opts $ssh_addr "sudo -u stack $run_opts $cos_dir/tripleo/check-openstack.sh"
