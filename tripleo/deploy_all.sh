9ab02a398c04fdb5730090c4e7fd6b108a03dc26
Andrey Pavlov <andrey.mp@gmail.com>
2017-08-15 08:59:22 +0300

    tripleo: export definition to allow to them in calling scripts

--- tripleo/deploy_all.sh
+++ tripleo/deploy_all.sh
@@ -22,7 +22,7 @@ ssh_addr="root@${ip_addr}"
 # TODO: place all definitions here
 
 # Dir with contrail packages
-CONTRAIL_PACKAGES_DIR=${CONTRAIL_PACKAGES_DIR:-'/home/root/contrail/latest'}
+export CONTRAIL_PACKAGES_DIR=${CONTRAIL_PACKAGES_DIR:-'/home/root/contrail/latest'}
 
 trap 'catch_errors $LINENO' ERR
 
