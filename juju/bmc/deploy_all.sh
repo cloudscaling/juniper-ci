f2ce315adb097cb803d09e491adb404181875767
Andrey Pavlov <andrey.mp@gmail.com>
2017-08-21 15:11:03 +0300

    juju-bmc: up build to 40 for 14.04 jjb: various fixed

--- juju/bmc/deploy_all.sh
+++ juju/bmc/deploy_all.sh
@@ -24,7 +24,7 @@ if [[ "$jver" == 1 ]] ; then
 fi
 
 declare -A BUILDS
-BUILDS=([mitaka]=22 [newton]=22)
+BUILDS=([mitaka]=40 [newton]=22)
 # for builds of R4.0 from 1 to 20 version is 4.0.0.0
 export CONTRAIL_VERSION="${CONTRAIL_VERSION:-4.0.1.0}"
 export SERIES="${SERIES:-xenial}"
