8f4ce071dddf71e3bc8590dc0c28381ac9e0e9d0
Andrey Pavlov <andrey.mp@gmail.com>
2017-08-23 14:08:23 +0300

    juju: tempest: use https scheme if SSL

--- juju/contrail/run-tempest.sh
+++ juju/contrail/run-tempest.sh
@@ -42,26 +42,30 @@ if [[ "$USE_SSL_OS" == "true" ]] ; then
   export OS_CACERT="$WORKSPACE/ssl/rootCA.pem"
 fi
 
-auth_ip=`get_machine_ip keystone`
-
 create_stackrc
 source $WORKSPACE/stackrc
 
+echo "INFO: Check/create images"
 create_virtualenv
 image_id=`create_image`
 image_id_alt=`create_image cirros_alt`
 create_flavors
 
 activate_venv
+echo "INFO: Get external network"
 network_id=`openstack network list --external -c ID -f value | head -1`
+echo "INFO: Create 'Member' role for unknown reason"
 openstack role create Member || /bin/true
+echo "INFO: Get network api extensions"
 api_ext=`openstack extension list --network -c Alias -f value | tr '\n' ','`
 
-if ! openstack project show demo1 ; then
+if ! openstack project show demo1 &>/dev/null ; then
+  echo "INFO: Creating project/user: demo1/demo1"
   openstack project create demo1
   openstack user create --project demo1 --password password demo1
 fi
-if ! openstack project show demo2 ; then
+if ! openstack project show demo2 &>/dev/null ; then
+  echo "INFO: Creating project/user: demo2/demo2"
   openstack project create demo2
   openstack user create --project demo2 --password password demo2
 fi
@@ -70,9 +74,10 @@ deactivate_venv
 cd $WORKSPACE/tempest
 rm -f *.xml
 
+echo "INFO: Prepare tempest.conf"
 CONF="$(pwd)/etc/tempest.conf"
 cp $my_dir/tempest/tempest.conf $CONF
-sed -i "s/%AUTH_IP%/$auth_ip/g" $CONF
+sed -i "s|%OS_AUTH_URL%|$OS_AUTH_URL|g" $CONF
 sed -i "s|%CAFILE%|$OS_CACERT|g" $CONF
 sed -i "s|%TEMPEST_DIR%|$(pwd)|g" $CONF
 sed -i "s/%IMAGE_ID%/$image_id/g" $CONF
@@ -80,21 +85,26 @@ sed -i "s/%IMAGE_ID_ALT%/$image_id_alt/g" $CONF
 sed -i "s/%NETWORK_ID%/$network_id/g" $CONF
 sed -i "s/%API_EXT%/$api_ext/g" $CONF
 
+echo "INFO: Prepare tempest requirements"
 activate_venv
 pip install -r requirements.txt
 pip install junitxml
 
+echo "INFO: Prepare tests list"
 tests=$(mktemp)
 tests_regex="(tempest\.api\.network)"
 python -m testtools.run discover -t ./ ./tempest/test_discover --list | grep -P "$tests_regex" > $tests
 tests_filtered=$(mktemp)
 python $my_dir/tempest/format_test_list.py $my_dir/tempest excludes.$VERSION $tests > $tests_filtered
 
+echo "INFO: Init testr"
 export OS_TEST_TIMEOUT=700
 [ -d .testrepository ] || testr init
 
 set +e
+echo "INFO: Run tempest tests"
 testr run --subunit --parallel --concurrency=2 --load-list=$tests_filtered | subunit-trace -n -f
+echo "INFO: Convert results"
 testr last --subunit | subunit-1to2 | python "$my_dir/../../tempest/subunit2jenkins.py" -o test_result.xml -s scaleio-openstack
 rm -f $tests $tests_filtered
 deactivate_venv
