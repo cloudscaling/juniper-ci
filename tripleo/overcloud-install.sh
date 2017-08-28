a9600537375c09d2ddb3ee1901d0d178f6443287
alexey-mr <alexey.morlang@gmail.com>
2017-08-28 19:36:01 +0300

    tripleo ci: use templates from dev repo if appropriate setting is enabled

--- tripleo/overcloud-install.sh
+++ tripleo/overcloud-install.sh
@@ -243,21 +243,22 @@ if [[ "$USE_DEVELOPMENT_PUPPETS" == 'true' ]] ; then
   artifact_opts="-e .tripleo/environments/deployment-artifacts.yaml"
 fi
 
-# TODO: replace personal repo with Juniper
-git_repo_ctht="juniper"
 # prepare tripleo heat templates
-#if [[ "$USE_DEVELOPMENT_PUPPETS" == 'true' ]] ; then
-  rm -rf ~/tripleo-heat-templates
-  cp -r /usr/share/openstack-tripleo-heat-templates/ ~/tripleo-heat-templates
-  rm -rf ~/contrail-tripleo-heat-templates
-  git clone https://github.com/${git_repo_ctht}/contrail-tripleo-heat-templates -b $git_branch
-  if [[ "$OPENSTACK_VERSION" == 'newton' ]] ; then
-    cp -r ~/contrail-tripleo-heat-templates/environments/contrail ~/tripleo-heat-templates/environments
-    cp -r ~/contrail-tripleo-heat-templates/puppet/services/network/* ~/tripleo-heat-templates/puppet/services/network
-  else
-    cp -r ~/contrail-tripleo-heat-templates/* ~/tripleo-heat-templates
-  fi
-#fi
+if [[ "$USE_DEVELOPMENT_PUPPETS" == 'true' ]] ; then
+  git_repo_ctht="cloudscaling"
+else
+  git_repo_ctht="juniper"
+fi
+rm -rf ~/tripleo-heat-templates
+cp -r /usr/share/openstack-tripleo-heat-templates/ ~/tripleo-heat-templates
+rm -rf ~/contrail-tripleo-heat-templates
+git clone https://github.com/${git_repo_ctht}/contrail-tripleo-heat-templates -b $git_branch
+if [[ "$OPENSTACK_VERSION" == 'newton' ]] ; then
+  cp -r ~/contrail-tripleo-heat-templates/environments/contrail ~/tripleo-heat-templates/environments
+  cp -r ~/contrail-tripleo-heat-templates/puppet/services/network/* ~/tripleo-heat-templates/puppet/services/network
+else
+  cp -r ~/contrail-tripleo-heat-templates/* ~/tripleo-heat-templates
+fi
 
 if [[ "$OPENSTACK_VERSION" == 'newton' ]] ; then
   role_file='tripleo-heat-templates/environments/contrail/roles_data.yaml'
