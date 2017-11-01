fd1a9ddb2d76fe03e74e34b09ae31b6a4ccceef3
Andrey Pavlov <andrey.mp@gmail.com>
2017-11-01 16:38:37 +0300

    helm: skip docker installation

--- contrail-helm/__run-openstack-helm-gate.sh
+++ contrail-helm/__run-openstack-helm-gate.sh
@@ -11,27 +11,28 @@ if [[ -x $(command -v apt-get 2>/dev/null) ]]; then
   sudo apt-get install -y --no-install-recommends mc git wget ntp docker.io jq
 elif [[ -x $(command -v yum 2>/dev/null) ]]; then
   sudo yum install -y epel-release
-  sudo yum install -y docker-latest jq mc git wget ntp 
-  sudo cp -f /usr/lib/systemd/system/docker-latest.service /etc/systemd/system/docker.service
-  sudo sed -i "s|/var/lib/docker-latest|/var/lib/docker|g" /etc/systemd/system/docker.service
-  sudo sed -i 's/^OPTIONS/#OPTIONS/g' /etc/sysconfig/docker-latest
-  sudo sed -i "s|^MountFlags=slave|MountFlags=share|g" /etc/systemd/system/docker.service
-  sudo sed -i "/--seccomp-profile/,+1 d" /etc/systemd/system/docker.service
-  echo "DOCKER_STORAGE_OPTIONS=--storage-driver=overlay" | sudo tee /etc/sysconfig/docker-latest-storage
-  sudo setenforce 0 || true
-  sudo systemctl daemon-reload
-  sudo systemctl restart docker
+  sudo yum install -y mc git wget ntp
+#  sudo yum install docker-latest jq
+#  sudo cp -f /usr/lib/systemd/system/docker-latest.service /etc/systemd/system/docker.service
+#  sudo sed -i "s|/var/lib/docker-latest|/var/lib/docker|g" /etc/systemd/system/docker.service
+#  sudo sed -i 's/^OPTIONS/#OPTIONS/g' /etc/sysconfig/docker-latest
+#  sudo sed -i "s|^MountFlags=slave|MountFlags=share|g" /etc/systemd/system/docker.service
+#  sudo sed -i "/--seccomp-profile/,+1 d" /etc/systemd/system/docker.service
+#  echo "DOCKER_STORAGE_OPTIONS=--storage-driver=overlay" | sudo tee /etc/sysconfig/docker-latest-storage
+#  sudo setenforce 0 || true
+#  sudo systemctl daemon-reload
+#  sudo systemctl restart docker
 else
   echo "ERROR: Unable to find apt-get or yum"
   exit 1
 fi
 
-sudo docker pull docker.io/opencontrail/contrail-controller-ubuntu16.04:4.0.2.0
-sudo docker pull docker.io/opencontrail/contrail-analyticsdb-ubuntu16.04:4.0.2.0
-sudo docker pull docker.io/opencontrail/contrail-analytics-ubuntu16.04:4.0.2.0
-sudo docker pull docker.io/opencontrail/contrail-kube-manager-ubuntu16.04:4.0.2.0
-sudo docker pull docker.io/opencontrail/contrail-agent-ubuntu16.04:4.0.2.0
-sudo docker pull docker.io/opencontrail/contrail-kubernetes-agent-ubuntu16.04:4.0.2.0
+#sudo docker pull docker.io/opencontrail/contrail-controller-ubuntu16.04:4.0.2.0
+#sudo docker pull docker.io/opencontrail/contrail-analyticsdb-ubuntu16.04:4.0.2.0
+#sudo docker pull docker.io/opencontrail/contrail-analytics-ubuntu16.04:4.0.2.0
+#sudo docker pull docker.io/opencontrail/contrail-kube-manager-ubuntu16.04:4.0.2.0
+#sudo docker pull docker.io/opencontrail/contrail-agent-ubuntu16.04:4.0.2.0
+#sudo docker pull docker.io/opencontrail/contrail-kubernetes-agent-ubuntu16.04:4.0.2.0
 
 git clone https://github.com/openstack/openstack-helm
 cd openstack-helm
