501ca605097af88ce7a94bbae985cdd0b7a49bf9
alexey-mr <alexey.morlang@gmail.com>
2017-11-07 12:34:22 +0300

    oshelm: merge from aws versions

--- contrail-helm/run-server-centos.sh
+++ contrail-helm/run-server-centos.sh
@@ -25,10 +25,10 @@ $my_dir/server/create-vm.sh centos ocata
 
 source "$my_dir/server/ssh-defs"
 
-$SCP "$my_dir/__ceph.repo" $SSH_DEST_BUILD:ceph.repo
 $SCP "$my_dir/__containers-build-centos.sh" $SSH_DEST_BUILD:containers-build-centos.sh
 timeout -s 9 120m $SSH_BUILD "DOCKER_CONTRAIL_URL=$DOCKER_CONTRAIL_URL ./containers-build-centos.sh"
 
+$SCP "$my_dir/__ceph.repo" $SSH_DEST:ceph.repo
 $SCP "$my_dir/__run-openstack-helm-gate.sh" $SSH_DEST:run-openstack-helm-gate.sh
 timeout -s 9 120m $SSH "CHANGE_REF=$CHANGE_REF OPENSTACK_HELM_URL=$OPENSTACK_HELM_URL ./run-openstack-helm-gate.sh $public_ip_build"
 
