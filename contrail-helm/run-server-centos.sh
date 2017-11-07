27796523b644e3f560ee2fa53cbe97cabe6cc363
alexey-mr <alexey.morlang@gmail.com>
2017-11-07 17:19:01 +0300

    oshelm: temporary disable helm running

--- contrail-helm/run-server-centos.sh
+++ contrail-helm/run-server-centos.sh
@@ -28,9 +28,9 @@ source "$my_dir/server/ssh-defs"
 $SCP "$my_dir/__containers-build-centos.sh" $SSH_DEST_BUILD:containers-build-centos.sh
 timeout -s 9 120m $SSH_BUILD "DOCKER_CONTRAIL_URL=$DOCKER_CONTRAIL_URL ./containers-build-centos.sh"
 
-$SCP "$my_dir/__ceph.repo" $SSH_DEST:ceph.repo
-$SCP "$my_dir/__run-openstack-helm-gate.sh" $SSH_DEST:run-openstack-helm-gate.sh
-timeout -s 9 120m $SSH "CHANGE_REF=$CHANGE_REF OPENSTACK_HELM_URL=$OPENSTACK_HELM_URL ./run-openstack-helm-gate.sh $public_ip_build"
+# $SCP "$my_dir/__ceph.repo" $SSH_DEST:ceph.repo
+# $SCP "$my_dir/__run-openstack-helm-gate.sh" $SSH_DEST:run-openstack-helm-gate.sh
+# timeout -s 9 120m $SSH "CHANGE_REF=$CHANGE_REF OPENSTACK_HELM_URL=$OPENSTACK_HELM_URL ./run-openstack-helm-gate.sh $public_ip_build"
 
 trap - ERR
 $my_dir/server/save-logs.sh
