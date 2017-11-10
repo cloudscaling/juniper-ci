b718eebb00851871e13fbc36b5d63c81acd529e0
alexey-mr <alexey.morlang@gmail.com>
2017-11-10 13:17:25 +0300

    k8s: added path to be able run ip tool via ssh

--- contrail-helm/__run-k8s-gate.sh
+++ contrail-helm/__run-k8s-gate.sh
@@ -4,60 +4,81 @@ export CONTRAIL_VERSION=${CONTRAIL_VERSION:-'4.0.2.0-35'}
 export EXTERNAL_K8S_AGENT=${EXTERNAL_K8S_AGENT:-'docker.io/opencontrail/contrail-kubernetes-agent-ubuntu16.04:4.0.1.0'}
 export DOCKER_REGISTRY_ADDR=${DOCKER_REGISTRY_ADDR:-''}
 
+# to find ip tool
+export PATH=${PATH}:/usr/sbin
+
+function log_info() {
+  echo "INFO: $@"
+}
+
+function log_error() {
+  echo "ERROR: $@"
+}
+
+log_info "setup k8s ..."
 pushd docker-contrail-4
 kubernetes/setup-k8s.sh
 
 iface=`ip -4 route list 0/0 | awk '{ print $5; exit }'`
 local_ip=`ip addr | grep $iface | grep 'inet ' | awk '{print $2}' | cut -d '/' -f 1`
+log_info "iface=$iface local_ip=$local_ip"
 
 docker_registry=$DOCKER_REGISTRY_ADDR
 if [[ -z "$docker_registry" ]] ; then
   docker_registry="${local_ip}:5000"
 fi
+log_info "docker_registry=$docker_registry"
 
 if [[ -n "$EXTERNAL_K8S_AGENT" ]] ; then
+  log_info "pull external contrail agent: $EXTERNAL_K8S_AGENT"
   docker pull $EXTERNAL_K8S_AGENT
   kubernetes_agent_fname=$(echo "$EXTERNAL_K8S_AGENT" | awk -F '/' '{print($NF)}')
   kubernetes_agent_name=$(echo $kubernetes_agent_fname | cut -d ':' -f 1)
+  log_info "set tag: ${docker_registry}/${kubernetes_agent_name}:${CONTRAIL_VERSION}"
   docker tag $EXTERNAL_K8S_AGENT ${docker_registry}/${kubernetes_agent_name}:${CONTRAIL_VERSION}
+  log_info "set tag: ${docker_registry}/${kubernetes_agent_fname}"
   docker tag $EXTERNAL_K8S_AGENT ${docker_registry}/${kubernetes_agent_fname}
 fi
 
 sed "s/^#\(.*=\)\(.*\)/\1${local_ip}/g" common.env.sample | sed "s/PHYSICAL_INTERFACE=.*/PHYSICAL_INTERFACE=${iface}/g"  > common.env
+log_info "common.env:"
+cat common.env
 
 pushd kubernetes/manifests/
 ./resolve-manifest.sh <contrail-micro.yaml >my-contrail-micro.yaml
+log_info "generated k8s contrail manifest my-contrail-micro.yaml:"
+cat my-contrail-micro.yaml
 popd
 
 popd
-
 
 function wait_cluster() {
   local name=$1
   local pods_rgx=$2
-  echo "Wait $name up.."
+  log_info "Wait $name up.."
   local total=0
   local running=0
   local i=0
   for (( i=0 ; i << 600 ; ++i )) ; do
     total=$(kubectl get pods --all-namespaces=true | grep -c "$pods_rgx")
     running=$(kubectl get pods --all-namespaces=true | grep "$pods_rgx" | grep -ic 'running')
-    echo "  components up: ${running}/${total}"
+    log_info "  components up: ${running}/${total}"
     if (( total != 0 && total == running )) ; then
-      echo "$name is running"
+      log_info "$name is running"
       break
     fi
   done
   if (( total != running )) ; then
-    echo "$name failed to run till timeout"
+    log_error "$name failed to run till timeout"
     exit -1
   fi
 }
 
+log_info "create Contrail cluster"
 kubectl create -f docker-contrail-4/kubernetes/manifests/my-contrail-micro.yaml
 wait_cluster "Contrail" "contrail\|zookeeper\|rabbit\|kafka\|redis"
 
-echo "Run test application: nginx"
+log_info "Run test application: nginx"
 cat <<EOF > test_app.yaml
 apiVersion: apps/v1beta1 # for versions before 1.8.0 use apps/v1beta1
 kind: Deployment
@@ -84,9 +105,10 @@ spec:
       - operator: "Exists"
         effect: "NoSchedule"
 EOF
-echo "Test application yaml:"
+log_info "Test application yaml:"
 cat test_app.yaml
 
+log_info "run test application"
 kubectl create -f test_app.yaml
 wait_cluster "nginx" "nginx"
 
