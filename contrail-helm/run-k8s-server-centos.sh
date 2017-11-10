82e5a12bd45b528b69ec765bd4164c768ef941fc
alexey-mr <alexey.morlang@gmail.com>
2017-11-10 19:20:41 +0300

    k8s: add contrail version in call of ssh scripts

--- contrail-helm/run-k8s-server-centos.sh
+++ contrail-helm/run-k8s-server-centos.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -ex
 
+export CONTRAIL_VERSION=${CONTRAIL_VERSION:-'4.0.2.0-35'}
+
 my_file="$(readlink -e "$0")"
 my_dir="$(dirname $my_file)"
 
@@ -26,10 +28,10 @@ $my_dir/server/create-vm.sh centos ocata
 source "$my_dir/server/ssh-defs"
 
 $SCP "$my_dir/__containers-build-centos.sh" $SSH_DEST_BUILD:containers-build-centos.sh
-timeout -s 9 120m $SSH_BUILD "DOCKER_CONTRAIL_URL=$DOCKER_CONTRAIL_URL ./containers-build-centos.sh"
+timeout -s 9 120m $SSH_BUILD "CONTRAIL_VERSION=$CONTRAIL_VERSION DOCKER_CONTRAIL_URL=$DOCKER_CONTRAIL_URL ./containers-build-centos.sh"
 
 $SCP "$my_dir/__run-k8s-gate.sh" $SSH_DEST:run-k8s-gate.sh
-timeout -s 9 120m $SSH "CHANGE_REF=$CHANGE_REF ./run-k8s-gate.sh"
+timeout -s 9 120m $SSH "CONTRAIL_VERSION=$CONTRAIL_VERSION CHANGE_REF=$CHANGE_REF ./run-k8s-gate.sh"
 
 trap - ERR
 $my_dir/server/save-logs.sh
