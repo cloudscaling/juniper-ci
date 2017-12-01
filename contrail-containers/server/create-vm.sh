46f04afb5b7535dd75de8e229e54ef9a7421511c
alexey-mr <alexey.morlang@gmail.com>
2017-12-01 22:06:25 +0300

    k8s: use hostnames in node_ip format.
    For now only this format is supported by rabbitmq

--- contrail-containers/server/create-vm.sh
+++ contrail-containers/server/create-vm.sh
@@ -120,7 +120,7 @@ for ip in ${ips[@]} ; do
   (( index+=1 ))
   cat <<EOF | ssh $SSH_OPTS root@${ip}
 set -x
-hname=${WAY}-${index}
+hname=\"node_\$(echo $ip | tr '.' '_')\"
 echo \$hname > /etc/hostname
 hostname \$hname
 domainname localdomain
