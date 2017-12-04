e4a678c7a19fb874ba35361c0d0dcf20166773fb
Andrey Pavlov <andrey.mp@gmail.com>
2017-12-04 10:44:32 +0300

    containers: revert adding /usr/sbin to PATH

--- contrail-containers/__build-containers.sh
+++ contrail-containers/__build-containers.sh
@@ -6,6 +6,8 @@ my_dir="$(dirname $my_file)"
 source $my_dir/functions
 
 prepare_build_machine
+# ip is located in /usr/sbin that is not in path...
+export PATH=${PATH}:/usr/sbin
 
 echo "INFO: Run setup-for-build  $(date)"
 
