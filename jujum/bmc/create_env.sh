58a9913b5dacc5677edb170be52afcad74fbdd70
Andrey Pavlov <andrey.mp@gmail.com>
2017-12-13 08:57:08 +0300

    jovs: fix installing of bgp plugin

--- jujum/bmc/create_env.sh
+++ jujum/bmc/create_env.sh
@@ -150,7 +150,7 @@ function run_compute() {
   echo "INFO: preparing compute $index $(date)"
   kernel_version=`juju ssh ubuntu@$ip uname -r 2>/dev/null | tr -d '\r'`
   juju ssh ubuntu@$ip "sudo apt-get -fy install mc wget apparmor-profiles openvswitch-switch" &>>$log_dir/apt.log
-  juju scp "$my_dir/50-cloud-init-compute-$SERIES.cfg" ubuntu@$ip:50-cloud-init.cfg 2>/dev/null
+  juju scp "$my_dir/files/50-cloud-init-compute-$SERIES.cfg" ubuntu@$ip:50-cloud-init.cfg 2>/dev/null
   juju ssh ubuntu@$ip "sudo cp ./50-cloud-init.cfg /etc/network/interfaces.d/50-cloud-init.cfg" 2>/dev/null
   juju ssh ubuntu@$ip "echo 'supersede routers 10.0.0.1;' | sudo tee -a /etc/dhcp/dhclient.conf"
   juju ssh ubuntu@$ip "sudo reboot" 2>/dev/null || /bin/true
@@ -169,7 +169,7 @@ function run_network() {
   echo "INFO: preparing network $index $(date)"
   kernel_version=`juju ssh ubuntu@$ip uname -r 2>/dev/null | tr -d '\r'`
   juju ssh ubuntu@$ip "sudo apt-get -fy install mc wget apparmor-profiles openvswitch-switch" &>>$log_dir/apt.log
-  juju scp "$my_dir/50-cloud-init-compute-$SERIES.cfg" ubuntu@$ip:50-cloud-init.cfg 2>/dev/null
+  juju scp "$my_dir/files/50-cloud-init-compute-$SERIES.cfg" ubuntu@$ip:50-cloud-init.cfg 2>/dev/null
   juju ssh ubuntu@$ip "sudo cp ./50-cloud-init.cfg /etc/network/interfaces.d/50-cloud-init.cfg" 2>/dev/null
   juju ssh ubuntu@$ip "echo 'supersede routers 10.0.0.1;' | sudo tee -a /etc/dhcp/dhclient.conf"
   juju ssh ubuntu@$ip "sudo reboot" 2>/dev/null || /bin/true
@@ -195,7 +195,7 @@ function run_controller() {
     juju ssh ubuntu@$ip "sudo sed -i -e 's/^USE_LXD_BRIDGE.*$/USE_LXD_BRIDGE=\"false\"/m' /etc/default/lxd-bridge" 2>/dev/null
     juju ssh ubuntu@$ip "sudo sed -i -e 's/^LXD_BRIDGE.*$/LXD_BRIDGE=\"br-$IF1\"/m' /etc/default/lxd-bridge" 2>/dev/null
   fi
-  juju scp "$my_dir/50-cloud-init-controller-$SERIES.cfg" ubuntu@$ip:50-cloud-init.cfg 2>/dev/null
+  juju scp "$my_dir/files/50-cloud-init-controller-$SERIES.cfg" ubuntu@$ip:50-cloud-init.cfg 2>/dev/null
   juju ssh ubuntu@$ip "sudo cp ./50-cloud-init.cfg /etc/network/interfaces.d/50-cloud-init.cfg" 2>/dev/null
   juju ssh ubuntu@$ip "echo 'supersede routers 10.0.0.1;' | sudo tee -a /etc/dhcp/dhclient.conf"
   juju ssh ubuntu@$ip "sudo reboot" 2>/dev/null || /bin/true
