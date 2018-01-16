a41855e1a95fded292f517bacf710cb465b971f7
Evgeny Kuznetsov <31624181+e-kuznetsov@users.noreply.github.com>
2018-01-16 14:05:53 +0300

    change liburcu2 download path

--- tripleo/__undercloud-install-1-as-root.sh
+++ tripleo/__undercloud-install-1-as-root.sh
@@ -159,7 +159,7 @@ fi
 #       It should be fixed by addind the package either into contrail distribution
 #       or into RedHat repo or by enabling additional repo.
 if [[ "$DPDK" == 'true' ]] ; then
-  curl -o /var/www/html/contrail/liburcu2-0.8.6-21.1.x86_64.rpm  ftp://ftp.icm.edu.pl/vol/rzm6/linux-opensuse/repositories/devel:/tools:/lttng/RedHat_RHEL-5/x86_64/liburcu2-0.8.6-21.1.x86_64.rpm
+  curl -o /var/www/html/contrail/liburcu2-0.8.6-21.1.x86_64.rpm  http://ftp5.gwdg.de/pub/opensuse/repositories/devel:/tools:/lttng/RedHat_RHEL-5/x86_64/liburcu2-0.8.6-21.1.x86_64.rpm
   update_contrail_repo='yes'
 fi
 
