424e3e1b4ae060b9e805aa6ed095ae979c9c3c90
Andrey Pavlov <andrey.mp@gmail.com>
2018-02-02 12:38:56 +0300

    contrainers: update cache usage condition

--- contrail-containers/__build-containers.sh
+++ contrail-containers/__build-containers.sh
@@ -15,7 +15,7 @@ cd contrail-container-builder/containers
 
 # there are 30 images (all images without base images)
 # TODO: make check not so strong, add check for version/distro/openstack-version
-if [ -d $HOME/containers-cache ] && [[ $(ls -l $HOME/containers-cache | grep 'contrail-' | wc -l) == '30' ]] ; then
+if [ -d $HOME/containers-cache ] && (( $(ls -l $HOME/containers-cache | grep 'contrail-' | wc -l) >= 24 )) ; then
   echo "INFO: using cached containers... $(date)"
   ./validate-docker.sh
   ./install-registry.sh
