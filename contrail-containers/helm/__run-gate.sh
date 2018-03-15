e09ce65aa84b1cf39fe3681e26430e3f5f5202ee
Andrey Pavlov <andrey.mp@gmail.com>
2018-03-15 14:19:36 +0300

    containers/helm: change links to current reviews

--- contrail-containers/helm/__run-gate.sh
+++ contrail-containers/helm/__run-gate.sh
@@ -55,7 +55,7 @@ git clone https://github.com/Juniper/openstack-helm-infra.git
 # Download contrail-helm-deployer code
 git clone https://github.com/Juniper/contrail-helm-deployer.git
 pushd contrail-helm-deployer
-git fetch https://review.opencontrail.org/Juniper/contrail-helm-deployer refs/changes/36/40636/2 && git checkout FETCH_HEAD
+git fetch https://review.opencontrail.org/Juniper/contrail-helm-deployer refs/changes/95/40695/1 && git checkout FETCH_HEAD
 popd
 
 export BASE_DIR=${WORKSPACE:-$(pwd)}
