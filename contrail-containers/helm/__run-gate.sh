43efde088d306ca962812066cd50e8c5008db09a
Andrey Pavlov <andrey.mp@gmail.com>
2018-03-16 14:59:42 +0300

    containers/helm: update links

--- contrail-containers/helm/__run-gate.sh
+++ contrail-containers/helm/__run-gate.sh
@@ -56,7 +56,7 @@ git clone https://github.com/Juniper/openstack-helm-infra.git
 # Download contrail-helm-deployer code
 git clone https://github.com/Juniper/contrail-helm-deployer.git
 pushd contrail-helm-deployer
-git fetch https://review.opencontrail.org/Juniper/contrail-helm-deployer refs/changes/88/40688/1 && git checkout FETCH_HEAD
+git fetch https://review.opencontrail.org/Juniper/contrail-helm-deployer refs/changes/43/40743/1 && git checkout FETCH_HEAD
 git pull --rebase origin master
 popd
 
