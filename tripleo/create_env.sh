64f9eb8e39a08381593411f8aa4337e92bb403c5
alexey-mr <alexey.morlang@gmail.com>
2018-03-29 16:44:42 +0300

    [tripleo] update parts list after qemu-nbd connect

--- tripleo/create_env.sh
+++ tripleo/create_env.sh
@@ -258,6 +258,11 @@ function _patch_image() {
   qemu-nbd -d $nbd_dev || true
   qemu-nbd -n -c $nbd_dev $image
   sleep 5
+  if [ ! -f ${nbd_dev}p1 ] ; then
+    echo "No dev ${nbd_dev}p1 after nbd mount, try to update manually"
+    partx -a $nbd_dev
+    lsblk
+  fi
   local ret=0
   local tmpdir=$(mktemp -d)
   mount ${nbd_dev}p1 $tmpdir || ret=1
