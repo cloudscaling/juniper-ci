ec939401bc459cfbaa799029791b81c7d1b42f9d
Andrey Pavlov <andrey.mp@gmail.com>
2018-04-05 22:49:27 +0300

    containers/ansible: fixes

--- contrail-containers/ansible/__run-gate.sh
+++ contrail-containers/ansible/__run-gate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 
-mkdir /root/.ssh && cp /.ssh/* /root/.ssh/ && chown root:root /root/.ssh
+mkdir -p /root/.ssh && cp /.ssh/* /root/.ssh/ && chown root:root /root/.ssh
 cd /root/contrail-ansible-deployer
 ansible-playbook -v -i inventory/ -e config_file=/root/contrail-ansible-deployer/instances.yaml playbooks/configure_instances.yml
 ansible-playbook -v -i inventory/ -e orchestrator=openstack -e config_file=/root/contrail-ansible-deployer/instances.yaml playbooks/install_contrail.yml
