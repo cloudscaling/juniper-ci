1d89fd8373148038aa3f6cf318cf6bd9939355af
Andrey Pavlov <andrey.mp@gmail.com>
2018-04-06 19:05:02 +0300

    containers/ansible: add debug output

--- contrail-containers/ansible/__run-gate.sh
+++ contrail-containers/ansible/__run-gate.sh
@@ -3,4 +3,4 @@
 mkdir -p /root/.ssh && cp /.ssh/* /root/.ssh/ && chown root:root /root/.ssh
 cd /root/contrail-ansible-deployer
 ansible-playbook -v -i inventory/ -e orchestrator=openstack -e config_file=/root/contrail-ansible-deployer/instances.yaml playbooks/configure_instances.yml
-ansible-playbook -v -i inventory/ -e orchestrator=openstack -e config_file=/root/contrail-ansible-deployer/instances.yaml playbooks/install_contrail.yml
+ansible-playbook -vvvv -i inventory/ -e orchestrator=openstack -e config_file=/root/contrail-ansible-deployer/instances.yaml playbooks/install_contrail.yml
