a49cac195aca1a4c80e4c32aa7d53c313fb6a7e1
Andrey Pavlov <andrey.mp@gmail.com>
2018-04-06 14:40:01 +0300

    containers/ansible: revert openstack

--- contrail-containers/ansible/__run-gate.sh
+++ contrail-containers/ansible/__run-gate.sh
@@ -2,5 +2,5 @@
 
 mkdir -p /root/.ssh && cp /.ssh/* /root/.ssh/ && chown root:root /root/.ssh
 cd /root/contrail-ansible-deployer
-ansible-playbook -v -i inventory/ -e orchestrator -e config_file=/root/contrail-ansible-deployer/instances.yaml playbooks/configure_instances.yml
-ansible-playbook -v -i inventory/ -e orchestrator -e config_file=/root/contrail-ansible-deployer/instances.yaml playbooks/install_contrail.yml
+ansible-playbook -v -i inventory/ -e orchestrator=openstack -e config_file=/root/contrail-ansible-deployer/instances.yaml playbooks/configure_instances.yml
+ansible-playbook -v -i inventory/ -e orchestrator=openstack -e config_file=/root/contrail-ansible-deployer/instances.yaml playbooks/install_contrail.yml
