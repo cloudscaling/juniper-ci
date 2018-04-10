5beac3f23cdd98ca5926f625268247ea8829ecea
Andrey Pavlov <andrey.mp@gmail.com>
2018-04-10 16:09:05 +0300

    containers/kolla: fix build.sh call params

--- contrail-containers/kolla/run.sh
+++ contrail-containers/kolla/run.sh
@@ -63,7 +63,7 @@ if [[ "$REGISTRY" == 'build' ]]; then
   $SCP -r "$WORKSPACE/contrail-container-builder" $SSH_USER@$build_ip:./
   set -o pipefail
   ssh_env="CONTRAIL_VERSION=$CONTRAIL_VERSION OPENSTACK_VERSION=$OPENSTACK_VERSION"
-  ssh_env=" LINUX_DISTR=$LINUX_DISTR CONTRAIL_INSTALL_PACKAGES_URL=$CONTRAIL_INSTALL_PACKAGES_URL"
+  ssh_env+=" LINUX_DISTR=$LINUX_DISTR CONTRAIL_INSTALL_PACKAGES_URL=$CONTRAIL_INSTALL_PACKAGES_URL"
   $SSH_CMD $SSH_USER@$build_ip "$ssh_env timeout -s 9 180m ./build-containers.sh" |& tee $WORKSPACE/logs/build.log
   set +o pipefail
   CONTAINER_REGISTRY="$build_ip:5000"
