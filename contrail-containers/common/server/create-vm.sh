d6d202b34ef035191df9108a3e824d4041f6b333
Andrey Pavlov <andrey.mp@gmail.com>
2018-04-14 20:13:34 +0300

    containers/server: remove cloud-init and python-requests
    due to conflicts with next stages

--- contrail-containers/common/server/create-vm.sh
+++ contrail-containers/common/server/create-vm.sh
@@ -223,6 +223,7 @@ EOM
   yum update -y &>>$logs_dir/yum.log
   yum install -y epel-release &>>$logs_dir/yum.log
   yum install -y mc git wget ntp ntpdate iptables iproute libxml2-utils python2.7 lsof python-pip python-devel gcc&>>$logs_dir/yum.log
+  yum remove -y python-requests cloud-init
   systemctl disable chronyd.service
   systemctl enable ntpd.service && systemctl start ntpd.service
 elif [[ "$ENVIRONMENT_OS" == 'ubuntu' ]]; then
