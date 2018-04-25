d98b4b4ea606dab9e1d3eec56dd1512cd6abd5ed
Andrey Pavlov <andrey.mp@gmail.com>
2018-04-25 14:34:55 +0300

    containers/ansible: save /etc/kolla to logs

--- contrail-containers/ansible/__run-gate.sh
+++ contrail-containers/ansible/__run-gate.sh
@@ -1,6 +1,16 @@
-#!/bin/bash -ex
+#!/bin/bash -x
 
 mkdir -p /root/.ssh && cp /.ssh/* /root/.ssh/ && chown root:root /root/.ssh
 cd /root/contrail-ansible-deployer
-ansible-playbook -v -i inventory/ -e config_file=/root/contrail-ansible-deployer/instances.yaml playbooks/configure_instances.yml
-ansible-playbook -v -i inventory/ -e orchestrator=openstack -e config_file=/root/contrail-ansible-deployer/instances.yaml playbooks/install_contrail.yml
+
+ansible-playbook -v -i inventory/ -e config_file=/root/contrail-ansible-deployer/instances.yaml playbooks/configure_instances.yml || ret=1
+cp -r /etc/kolla /root/logs/
+if [[ "$ret" == '1' ]]; then
+  exit 1
+fi
+
+ansible-playbook -v -i inventory/ -e orchestrator=openstack -e config_file=/root/contrail-ansible-deployer/instances.yaml playbooks/install_contrail.yml || ret=1
+# copy needed info to /root/logs
+if [[ "$ret" == '1' ]]; then
+  exit 1
+fi
