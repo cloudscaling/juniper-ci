7993c276d3049b6687dce20690d3d5de8fdb7b95
Andrey Pavlov <andrey.mp@gmail.com>
2018-05-11 13:09:59 +0300

    containers/jjb: add distr to packages URL

--- contrail-containers/ak8s/__run-gate.sh
+++ contrail-containers/ak8s/__run-gate.sh
@@ -4,10 +4,7 @@ mkdir -p /root/.ssh && cp /.ssh/* /root/.ssh/ && chown root:root /root/.ssh
 cd /root
 
 function save_logs() {
-  mkdir -p /root/logs/kolla
-  cp -r /root/contrail-kolla-ansible/etc/kolla/globals.yml /root/logs/kolla/
-  cp -r /root/contrail-kolla-ansible/ansible/host_vars /root/logs/kolla/
-  chmod -R a+rw /root/logs/kolla
+  :
 }
 
 trap 'catch_errors $LINENO' ERR
@@ -21,8 +18,8 @@ function catch_errors() {
 }
 
 cd contrail-ansible-deployer
-ansible-playbook -v -i inventory/ -e config_file=/root/contrail-ansible-deployer/instances.yaml -e kolla_dir=/tmp playbooks/configure_instances.yml
-ansible-playbook -v -i inventory/ -e orchestrator=openstack -e config_file=/root/contrail-ansible-deployer/instances.yaml playbooks/install_contrail.yml
+ansible-playbook -v -e config_file=/root/contrail-ansible-deployer/instances.yaml playbooks/configure_instances.yml
+ansible-playbook -v -e orchestrator=kubernetes -e config_file=/root/contrail-ansible-deployer/instances.yaml playbooks/install_contrail.yml
 
 trap - ERR
 save_logs
