e25a8f6ef71299de990e8fc339258bfca1121de0
Andrey Pavlov <andrey.mp@gmail.com>
2018-05-24 14:09:03 +0300

    containers/ansible: fix calls to ansible

--- contrail-containers/ajupg/__run-gate.sh
+++ contrail-containers/ajupg/__run-gate.sh
@@ -4,5 +4,5 @@ mkdir -p /root/.ssh && cp /.ssh/* /root/.ssh/ && chown root:root /root/.ssh
 cd /root
 
 cd contrail-ansible-deployer
-ansible-playbook -v -e orchestrator=openstack -e config_file=/root/contrail-ansible-deployer/instances.yaml -e kolla_dir=/tmp playbooks/configure_instances.yml
+ansible-playbook -v -e orchestrator=openstack -e config_file=/root/contrail-ansible-deployer/instances.yaml playbooks/configure_instances.yml
 ansible-playbook -v -e orchestrator=openstack -e config_file=/root/contrail-ansible-deployer/instances.yaml playbooks/install_contrail.yml
