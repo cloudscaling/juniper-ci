58c9318c51d2eba39c2257657965097c4c81e844
Andrey Pavlov <andrey.mp@gmail.com>
2018-05-31 17:59:21 +0300

    containers/ansible: use one interface for mgmt and api

--- contrail-containers/ansible/run.sh
+++ contrail-containers/ansible/run.sh
@@ -54,10 +54,10 @@ fi
 # deploy cloud
 source "$my_dir/../common/${HOST}/${ENVIRONMENT_OS}"
 
-IP_VM_01=`echo $nodes_cont_ips | cut -d ' ' -f 1`
-IP_VM_02=`echo $nodes_cont_ips | cut -d ' ' -f 2`
-IP_VM_03=`echo $nodes_cont_ips | cut -d ' ' -f 3`
-IP_VM_04=`echo $nodes_comp_ips | cut -d ' ' -f 1`
+IP_VM_01=`echo $nodes_cont_ips_1 | cut -d ' ' -f 1`
+IP_VM_02=`echo $nodes_cont_ips_1 | cut -d ' ' -f 2`
+IP_VM_03=`echo $nodes_cont_ips_1 | cut -d ' ' -f 3`
+IP_VM_04=`echo $nodes_comp_ips_1 | cut -d ' ' -f 1`
 
 IP_VIP=10.$((NET_BASE_PREFIX+1)).$JOB_RND.254
 IP_CONT_01=`echo ${nodes_cont_ips_1} | cut -d ' ' -f 1`
@@ -69,7 +69,7 @@ IP2_CONT_01=`echo ${nodes_cont_ips_2} | cut -d ' ' -f 1`
 IP2_CONT_02=`echo ${nodes_cont_ips_2} | cut -d ' ' -f 2`
 IP2_CONT_03=`echo ${nodes_cont_ips_2} | cut -d ' ' -f 3`
 VROUTER_IF=ens5
-VROUTER_GW=10.$((NET_BASE_PREFIX+2)).$JOB_RND.254.1
+VROUTER_GW=10.$((NET_BASE_PREFIX+2)).$JOB_RND.1
 
 config=$WORKSPACE/contrail-ansible-deployer/instances.yaml
 templ=$(cat $my_dir/instances.yaml.tmpl)
