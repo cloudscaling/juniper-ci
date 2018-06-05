4171ca02f7b154b5bc78bd2523765c633e255db7
Andrey Pavlov <andrey.mp@gmail.com>
2018-06-05 21:46:55 +0300

    container/ansible: use third nic for os_vip

--- contrail-containers/ansible/run.sh
+++ contrail-containers/ansible/run.sh
@@ -59,13 +59,14 @@ IP_VM_02=`echo $nodes_cont_ips | cut -d ' ' -f 2`
 IP_VM_03=`echo $nodes_cont_ips | cut -d ' ' -f 3`
 IP_VM_04=`echo $nodes_comp_ips | cut -d ' ' -f 1`
 
-IP_VIP=10.$((NET_BASE_PREFIX+1)).$JOB_RND.254
+IP_VIP=10.$NET_BASE_PREFIX.$JOB_RND.254
 IP_CONT_01=`echo ${nodes_cont_ips} | cut -d ' ' -f 1`
 IP_CONT_02=`echo ${nodes_cont_ips} | cut -d ' ' -f 2`
 IP_CONT_03=`echo ${nodes_cont_ips} | cut -d ' ' -f 3`
 API_IF=ens3
 
-OS_VIP=10.$((NET_BASE_PREFIX+2)).$JOB_RND.254
+OS_VIP=10.$((NET_BASE_PREFIX+1)).$JOB_RND.254
+
 IP2_CONT_01=`echo ${nodes_cont_ips_2} | cut -d ' ' -f 1`
 IP2_CONT_02=`echo ${nodes_cont_ips_2} | cut -d ' ' -f 2`
 IP2_CONT_03=`echo ${nodes_cont_ips_2} | cut -d ' ' -f 3`
