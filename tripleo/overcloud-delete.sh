7a4193ccda0ab5bb01347490ba39efecd1c831f9
alexey-mr <alexey.morlang@gmail.com>
2018-07-02 22:05:13 +0300

    [tripleo] fixed syntax for stack deletion

--- tripleo/overcloud-delete.sh
+++ tripleo/overcloud-delete.sh
@@ -8,7 +8,7 @@ source ~/stackrc
 count=0
 while openstack stack list | grep -q overcloud ; do
   if ! openstack stack list | grep overcloud | grep -qi 'delete' ; then
-    openstack stack delete -y overcloud || true
+    openstack stack delete --yes overcloud || true
   fi
   if (( count > 300 )) ; then
     echo "WARNING: failed to wait while stack is deleted"
