fcbdc12ff592c6735bbc02edd7adad8c4ed97f23
Andrey Pavlov <andrey.mp@gmail.com>
2018-07-18 15:45:29 +0300

    juju/bmc: set second iface always static

--- juju/bmc-contrail/files/__prepare-network.sh
+++ juju/bmc-contrail/files/__prepare-network.sh
@@ -1,20 +1,8 @@
 #!/bin/bash -e
 
-iface=$1
-
-if ! grep -q "iface $iface inet static" ./50-cloud-init.cfg ; then
-  echo "ERROR: static interface $iface couldn't be found in ./50-cloud-init.cfg"
-  cat ./50-cloud-init.cfg
-  exit 1
-fi
-
-address=`ip addr show $iface | awk '/inet /{print $2}' | cut -d '/' -f 1`
-gw=`route -n | grep "^0\.0\.0\.0.*${iface}$" | awk '{print $2}'`
+address=$1
 
 sed -i "s/<address>/$address/g" ./50-cloud-init.cfg
-if [[ -n "$gw" ]]; then
-  sed -i "s/#gw <gw>/gw $gw/g" ./50-cloud-init.cfg
-fi
 
 sudo cp ./50-cloud-init.cfg /etc/network/interfaces.d/50-cloud-init.cfg
 series=`lsb_release -cs`
