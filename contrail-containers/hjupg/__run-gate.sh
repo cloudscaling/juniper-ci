cbe52ce5620d46cec99ab7af474a5f39404e7462
Andrey Pavlov <andrey.mp@gmail.com>
2018-08-06 23:56:15 +0300

    containers: add juju-helm upgrade

---
+++ contrail-containers/hjupg/__run-gate.sh
@@ -0,0 +1,267 @@
+#!/bin/bash -e
+
+my_file="$(readlink -e "$0")"
+my_dir="$(dirname $my_file)"
+
+mkdir -p $my_dir/logs
+source "$my_dir/cloudrc"
+
+AAA_MODE=${AAA_MODE:-cloud-admin}
+tag="$CONTRAIL_VERSION"
+
+# tune some host settings
+sudo sysctl -w vm.max_map_count=1048575
+
+if [[ "$REGISTRY_INSECURE" == '1' ]] ; then
+  sudo mkdir -p /etc/docker
+  cat | sudo tee /etc/docker/daemon.json << EOF
+{
+    "insecure-registries": ["$CONTAINER_REGISTRY"]
+}
+EOF
+fi
+
+if [[ -x $(command -v apt-get 2>/dev/null) ]]; then
+  HOST_OS='ubuntu'
+elif [[ -x $(command -v yum 2>/dev/null) ]]; then
+  HOST_OS='centos'
+else
+  echo "ERROR: Unable to find apt-get or yum"
+  exit 1
+fi
+
+echo "INFO: Preparing instances"
+if [ "x$HOST_OS" == "xcentos" ]; then
+  # ip is located in /usr/sbin that is not in path...
+  export PATH=${PATH}:/usr/sbin
+fi
+
+export BASE_DIR=/opt
+mkdir -p $BASE_DIR
+export OSH_PATH=${BASE_DIR}/openstack-helm
+export OSH_INFRA_PATH=${BASE_DIR}/openstack-helm-infra
+export CHD_PATH=${BASE_DIR}/contrail-helm-deployer
+
+cat <<EOF > $OSH_INFRA_PATH/tools/gate/devel/multinode-vars.yaml
+version:
+  kubernetes: v1.8.3
+  helm: v2.7.2
+  cni: v0.6.0
+kubernetes:
+  network:
+    default_device: ens3
+  cluster:
+    cni: calico
+    pod_subnet: 192.168.0.0/16
+    domain: cluster.local
+EOF
+if [[ "$REGISTRY_INSECURE" == '1' ]] ; then
+  cat <<EOF >> $OSH_INFRA_PATH/tools/gate/devel/multinode-vars.yaml
+docker:
+  insecure_registries:
+    - "$CONTAINER_REGISTRY"
+EOF
+fi
+
+if [[ "${SSL_ENABLE,,}" == 'true' ]] ; then
+  cat <<EOF >> $OSH_INFRA_PATH/tools/gate/devel/multinode-vars.yaml
+tls_config:
+  generate: true
+  organization: Contrail
+  cert_file: "/etc/contrail/ssl/certs/server.pem"
+  key_file: "/etc/contrail/ssl/private/server-privkey.pem"
+  ca_file: "/etc/contrail/ssl/certs/ca-cert.pem"
+EOF
+fi
+
+cd ${OSH_PATH}
+./tools/deployment/developer/common/001-install-packages-opencontrail.sh
+
+cat > $OSH_INFRA_PATH/tools/gate/devel/multinode-inventory.yaml <<EOF
+all:
+  children:
+EOF
+
+key_file="$my_dir/kp"
+ips=($nodes_ips)
+ip="${ips[0]}"
+name=`echo node_$ip | tr '.' '_'`
+cat >> $OSH_INFRA_PATH/tools/gate/devel/multinode-inventory.yaml <<EOF
+    primary:
+      hosts:
+        $name:
+          ansible_port: 22
+          ansible_host: $ip
+          ansible_user: $SSH_USER
+          ansible_ssh_extra_args: -o StrictHostKeyChecking=no
+EOF
+if [ -f $key_file ]; then
+cat >> $OSH_INFRA_PATH/tools/gate/devel/multinode-inventory.yaml <<EOF
+          ansible_ssh_private_key_file: $key_file
+EOF
+fi
+cat >> $OSH_INFRA_PATH/tools/gate/devel/multinode-inventory.yaml <<EOF
+    nodes:
+      hosts:
+EOF
+for ip in ${ips[@]:1} ; do
+  name=`echo node_$ip | tr '.' '_'`
+  cat >> $OSH_INFRA_PATH/tools/gate/devel/multinode-inventory.yaml <<EOF
+        $name:
+          ansible_port: 22
+          ansible_host: $ip
+          ansible_user: $SSH_USER
+          ansible_ssh_extra_args: -o StrictHostKeyChecking=no
+EOF
+  if [ -f $key_file ]; then
+  cat >> $OSH_INFRA_PATH/tools/gate/devel/multinode-inventory.yaml <<EOF
+          ansible_ssh_private_key_file: $key_file
+EOF
+  fi
+done
+
+set -x
+cd ${OSH_INFRA_PATH}
+make dev-deploy setup-host multinode
+make dev-deploy k8s multinode
+
+nslookup kubernetes.default.svc.cluster.local || /bin/true
+kubectl get nodes -o wide
+
+# names are assigned by kubernetes. use the same algorithm to generate name.
+for ip in $nodes_cont_ips ; do
+  name="node-$(echo $ip | tr '.' '-').localdomain"
+  kubectl label node $name --overwrite openstack-compute-node=disable
+  kubectl label node $name opencontrail.org/controller=enabled
+done
+for ip in $nodes_comp_ips ; do
+  name="node-$(echo $ip | tr '.' '-').localdomain"
+  kubectl label node $name --overwrite openstack-control-plane=disable
+  if [[ "$AGENT_MODE" == "dpdk" ]]; then
+    kubectl label node $name opencontrail.org/vrouter-dpdk=enabled
+  else
+    kubectl label node $name opencontrail.org/vrouter-kernel=enabled
+  fi
+done
+
+#cd ${OSH_PATH}
+#extra_neutron_args=''
+#if [[ "$AAA_MODE" == 'rbac' ]]; then
+#  extra_neutron_args="--values ./tools/overrides/backends/opencontrail/neutron-rbac.yaml"
+#fi
+#export OSH_EXTRA_HELM_ARGS_NEUTRON="$extra_neutron_args --set images.tags.opencontrail_neutron_init=$CONTAINER_REGISTRY/contrail-openstack-neutron-init:$tag"
+#echo "INFO: extra neutron args: $OSH_EXTRA_HELM_ARGS_NEUTRON"
+#extra_nova_args=''
+#if [[ "$OPENSTACK_VERSION" == 'ocata' ]]; then
+#  extra_nova_args="--set compute_patch=true"
+#fi
+#export OSH_EXTRA_HELM_ARGS_NOVA="$extra_nova_args --set images.tags.opencontrail_compute_init=$CONTAINER_REGISTRY/contrail-openstack-compute-init:$tag"
+#echo "INFO: extra nova args: $OSH_EXTRA_HELM_ARGS_NOVA"
+#export OSH_EXTRA_HELM_ARGS_HEAT="--set images.tags.opencontrail_heat_init=$CONTAINER_REGISTRY/contrail-openstack-heat-init:$tag"
+#echo "INFO: extra heat args: $OSH_EXTRA_HELM_ARGS_HEAT"
+#
+#./tools/deployment/multinode/010-setup-client.sh
+#./tools/deployment/multinode/021-ingress-opencontrail.sh
+#./tools/deployment/multinode/030-ceph.sh
+#./tools/deployment/multinode/040-ceph-ns-activate.sh
+#./tools/deployment/multinode/050-mariadb.sh
+#./tools/deployment/multinode/060-rabbitmq.sh
+#./tools/deployment/multinode/070-memcached.sh
+#./tools/deployment/multinode/080-keystone.sh
+#./tools/deployment/multinode/090-ceph-radosgateway.sh
+#./tools/deployment/multinode/100-glance.sh
+#./tools/deployment/multinode/110-cinder.sh
+#./tools/deployment/multinode/131-libvirt-opencontrail.sh
+
+cd $CHD_PATH
+make
+kubectl replace -f ${CHD_PATH}/rbac/cluster-admin.yaml
+
+controller_nodes=`echo $nodes_cont_ips | tr ' ' ','`
+control_nodes=`echo $nodes_cont_ips_1 | tr ' ' ','`
+tee /tmp/contrail.yaml << EOF
+global:
+  images:
+    tags:
+      kafka: "$CONTAINER_REGISTRY/contrail-external-kafka:$tag"
+      cassandra: "$CONTAINER_REGISTRY/contrail-external-cassandra:$tag"
+      redis: "redis:4.0.2"
+      zookeeper: "$CONTAINER_REGISTRY/contrail-external-zookeeper:$tag"
+      contrail_control: "$CONTAINER_REGISTRY/contrail-controller-control-control:$tag"
+      control_dns: "$CONTAINER_REGISTRY/contrail-controller-control-dns:$tag"
+      control_named: "$CONTAINER_REGISTRY/contrail-controller-control-named:$tag"
+      config_api: "$CONTAINER_REGISTRY/contrail-controller-config-api:$tag"
+      config_devicemgr: "$CONTAINER_REGISTRY/contrail-controller-config-devicemgr:$tag"
+      config_schema_transformer: "$CONTAINER_REGISTRY/contrail-controller-config-schema:$tag"
+      config_svcmonitor: "$CONTAINER_REGISTRY/contrail-controller-config-svcmonitor:$tag"
+      webui_middleware: "$CONTAINER_REGISTRY/contrail-controller-webui-job:$tag"
+      webui: "$CONTAINER_REGISTRY/contrail-controller-webui-web:$tag"
+      analytics_api: "$CONTAINER_REGISTRY/contrail-analytics-api:$tag"
+      contrail_collector: "$CONTAINER_REGISTRY/contrail-analytics-collector:$tag"
+      analytics_alarm_gen: "$CONTAINER_REGISTRY/contrail-analytics-alarm-gen:$tag"
+      analytics_query_engine: "$CONTAINER_REGISTRY/contrail-analytics-query-engine:$tag"
+      analytics_snmp_collector: "$CONTAINER_REGISTRY/contrail-analytics-snmp-collector:$tag"
+      contrail_topology: "$CONTAINER_REGISTRY/contrail-analytics-topology:$tag"
+      build_driver_init: "$CONTAINER_REGISTRY/contrail-vrouter-kernel-build-init:$tag"
+      vrouter_agent: "$CONTAINER_REGISTRY/contrail-vrouter-agent:$tag"
+      vrouter_init_kernel: "$CONTAINER_REGISTRY/contrail-vrouter-kernel-init:$tag"
+      vrouter_dpdk: "$CONTAINER_REGISTRY/contrail-vrouter-agent-dpdk:$tag"
+      vrouter_init_dpdk: "$CONTAINER_REGISTRY/contrail-vrouter-kernel-init-dpdk:$tag"
+      dpdk_watchdog: "$CONTAINER_REGISTRY/contrail-vrouter-net-watchdog:$tag"
+      nodemgr: "$CONTAINER_REGISTRY/contrail-nodemgr:$tag"
+      contrail_status: "$CONTAINER_REGISTRY/contrail-status:$tag"
+      node_init: "$CONTAINER_REGISTRY/contrail-node-init:$tag"
+      dep_check: quay.io/stackanetes/kubernetes-entrypoint:v0.2.1
+  contrail_env:
+    CONTROLLER_NODES: $controller_nodes
+    CONTROL_NODES: $control_nodes
+    LOG_LEVEL: SYS_DEBUG
+    CLOUD_ORCHESTRATOR: openstack
+    AAA_MODE: $AAA_MODE
+    VROUTER_GATEWAY: 10.$((NET_BASE_PREFIX+1)).$JOB_RND.1
+    SSL_ENABLE: $SSL_ENABLE
+    JVM_EXTRA_OPTS: "-Xms1g -Xmx2g"
+    BGP_PORT: "1179"
+    CONFIG_DATABASE_NODEMGR__DEFAULTS__minimum_diskGB: "2"
+    DATABASE_NODEMGR__DEFAULTS__minimum_diskGB: "2"
+    KEYSTONE_AUTH_ADMIN_TENANT=admin
+    KEYSTONE_AUTH_ADMIN_USER=admin
+    KEYSTONE_AUTH_ADMIN_PASSWORD=password
+    KEYSTONE_AUTH_PROJECT_DOMAIN_NAME=admin_domain
+    KEYSTONE_AUTH_USER_DOMAIN_NAME=admin_domain
+    KEYSTONE_AUTH_URL_VERSION=/v3
+    KEYSTONE_AUTH_HOST=${AUTH_IP}
+    KEYSTONE_AUTH_PROTO=http
+    IPFABRIC_SERVICE_HOST=${METADATA_IP}
+    METADATA_PROXY_SECRET=${METADATA_PROXY_SECRET}
+EOF
+
+helm install --name contrail ${CHD_PATH}/contrail --namespace=contrail --values=/tmp/contrail.yaml
+${OSH_PATH}/tools/deployment/common/wait-for-pods.sh contrail
+
+cd ${OSH_PATH}
+
+#./tools/deployment/multinode/141-compute-kit-opencontrail.sh
+
+# workaround steps. remove later.
+#make build-helm-toolkit
+#make build-heat
+#./tools/deployment/developer/nfs/091-heat-opencontrail.sh
+
+# lets wait for services
+sleep 60
+sudo contrail-status
+
+sudo apt-get install -fy virtualenv &>> $my_dir/logs/apt.log
+export OS_CLOUD=openstack_helm
+
+cd $my_dir
+source $my_dir/check-functions
+virtualenv .venv
+source .venv/bin/activate
+pip install python-openstackclient &>> $my_dir/logs/pip.log || /bin/true
+
+prepare_openstack && check_simple_instance
+deactivate
+
+exit $err
---
+++ contrail-containers/hjupg/run.sh
@@ -0,0 +1,92 @@
+#!/bin/bash -e
+
+my_file="$(readlink -e "$0")"
+my_dir="$(dirname $my_file)"
+
+if [[ "$CLEAN_BEFORE" == 'true' || "$CLEAN_BEFORE" == 'clean_and_exit' ]] ; then
+  $my_dir/../common/${HOST}/cleanup.sh || /bin/true
+  if [[ "$CLEAN_BEFORE" == 'clean_and_exit' ]] ; then
+    exit
+  fi
+fi
+
+rm -rf "$WORKSPACE/logs"
+mkdir -p "$WORKSPACE/logs"
+
+# definition for job deployment
+source $my_dir/${HOST}-defs
+source $my_dir/../common/functions
+
+# it should fail if Juju deployment is not found
+source $my_dir/../../juju/bmc-contrail/functions
+juju-status-tabular
+# ==============================================
+
+$my_dir/../common/${HOST}/create-vm.sh
+source "$my_dir/../common/${HOST}/ssh-defs"
+
+trap 'catch_errors $LINENO' ERR
+function catch_errors() {
+  local exit_code=$?
+  echo "Line: $1  Error=$exit_code  Command: '$(eval echo $BASH_COMMAND)'"
+
+  save_logs '2,3'
+  if [[ "$CLEAN_ENV" == 'always' ]] ; then
+    $my_dir/../common/${HOST}/cleanup.sh
+  fi
+
+  exit $exit_code
+}
+
+run_env=''
+if [[ "$REGISTRY" == 'build' ]]; then
+  $SCP "$my_dir/../__build-containers.sh" $SSH_USER@$build_ip:build-containers.sh
+  $SCP -r "$WORKSPACE/contrail-container-builder" $SSH_USER@$build_ip:./
+  set -o pipefail
+  ssh_env="CONTRAIL_VERSION=$CONTRAIL_VERSION OPENSTACK_VERSION=$OPENSTACK_VERSION"
+  ssh_env+=" LINUX_DISTR=$LINUX_DISTR CONTRAIL_INSTALL_PACKAGES_URL=$CONTRAIL_INSTALL_PACKAGES_URL"
+  $SSH_CMD $SSH_USER@$build_ip "$ssh_env timeout -s 9 180m ./build-containers.sh" |& tee $WORKSPACE/logs/build.log
+  set +o pipefail
+  run_env="CONTAINER_REGISTRY=$build_ip:5000 CONTRAIL_VERSION=$OPENSTACK_VERSION-$CONTRAIL_VERSION REGISTRY_INSECURE=1"
+elif [[ "$REGISTRY" == 'opencontrailnightly' ]]; then
+  run_env="CONTAINER_REGISTRY=opencontrailnightly CONTRAIL_VERSION=latest REGISTRY_INSECURE=0"
+else
+  echo "ERROR: unsupported REGISTRY = $REGISTRY"
+  exit 1
+fi
+
+# from juju
+AUTH_IP=`get_machine_ip keystone`
+METADATA_IP='127.0.0.1'
+# use machine 0 as we know that this is compute
+METADATA_PROXY_SECRET=`juju ssh 0 sudo grep metadata_proxy_secret /etc/contrail/contrail-vrouter-agent.conf 2>/dev/null | cut -d '=' -f 2 | tr -d ' '`
+
+# clone repos to all nodes
+for ip in $nodes_ips ; do
+  echo "INFO: some debug info about node $i"
+  $SSH_CMD $SSH_USER@$ip "hostname -i ; cat /etc/hosts"
+
+  echo "INFO: clone helm repos to node $ip"
+  $SSH_CMD $SSH_USER@$ip "sudo mkdir -p /opt && sudo chown $SSH_USER /opt"
+  for repo in 'openstack-helm' 'openstack-helm-infra' 'contrail-helm-deployer' ; do
+    $SSH_CMD $SSH_USER@$ip "git clone https://github.com/Juniper/${repo}.git /opt/$repo"
+    if echo "$PATCHSET_LIST" | grep -q "/${repo} " ; then
+      patchset=`echo "$PATCHSET_LIST" | grep "/${repo} "`
+      $SSH_CMD $SSH_USER@$ip "cd /opt/$repo ; $patchset ; git pull --rebase origin master"
+    fi
+  done
+done
+
+$SCP "$WORKSPACE/cloudrc" $SSH_USER@$master_ip:cloudrc
+$SCP "$my_dir/../common/check-functions" $SSH_USER@$master_ip:check-functions
+$SCP "$my_dir/__run-gate.sh" $SSH_USER@$master_ip:run-gate.sh
+run_env+=" OPENSTACK_VERSION=$OPENSTACK_VERSION LINUX_DISTR=$LINUX_DISTR"
+run_env+=" AGENT_MODE=$AGENT_MODE SSL_ENABLE=$SSL_ENABLE"
+run_env+=" AUTH_IP=$AUTH_IP METADATA_IP=$METADATA_IP METADATA_PROXY_SECRET=$METADATA_PROXY_SECRET"
+timeout -s 9 120m $SSH_CMD $SSH_USER@$master_ip "$run_env ./run-gate.sh"
+
+trap - ERR
+save_logs '2,3'
+if [[ "$CLEAN_ENV" == 'always' || "$CLEAN_ENV" == 'on_success' ]] ; then
+  $my_dir/../common/${HOST}/cleanup.sh
+fi
