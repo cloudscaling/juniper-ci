f2c5d394f9ca8332c9915fcd858f9e3d4118cd7f
tikitavi <rtikitavi@gmail.com>
2018-08-23 12:26:12 +0300

    juju: add R5 branch for deployment on aws

---
+++ juju/aws-contrail-R5/deploy-all.sh
@@ -0,0 +1,111 @@
+#!/bin/bash -e
+
+inner_script="${1:-deploy-manual.sh}"
+if [[ $# != 0 ]] ; then
+  shift
+  script_params="$@"
+fi
+
+my_file="$(readlink -e "$0")"
+my_dir="$(dirname $my_file)"
+source "$my_dir/../common/functions"
+
+if [[ "$CLEAN_BEFORE" == 'true' || "$CLEAN_BEFORE" == 'clean_and_exit' ]] ; then
+  cleanup_environment
+  if [[ "$CLEAN_BEFORE" == 'clean_and_exit' ]] ; then
+    exit
+  fi
+fi
+
+log_dir="$WORKSPACE/logs"
+if [ -d $log_dir ] ; then
+  chmod -R u+w "$log_dir"
+  rm -rf "$log_dir"
+fi
+mkdir "$log_dir"
+
+if [[ "$jver" == 1 ]] ; then
+  echo "ERROR: only juju 2.0 and higher supports resources. Please install and use juju 2.0 or higher."
+  exit 1
+fi
+
+export SERIES="${SERIES:-xenial}"
+export VERSION="${VERSION:-ocata}"
+export OPENSTACK_ORIGIN="cloud:$SERIES-$VERSION"
+export DEPLOY_AS_HA_MODE="${DEPLOY_AS_HA_MODE:-false}"
+export USE_SSL_OS="${USE_SSL_OS:-false}"
+export USE_SSL_CONTRAIL="${USE_SSL_CONTRAIL:-false}"
+export USE_ADDITIONAL_INTERFACE="${USE_ADDITIONAL_INTERFACE:-false}"
+export USE_EXTERNAL_RABBITMQ="${USE_EXTERNAL_RABBITMQ:-false}"
+export AAA_MODE=${AAA_MODE:-rbac}
+
+export PASSWORD=${PASSWORD:-'password'}
+
+echo "INFO: Date: $(date)"
+echo "INFO: Starting deployment process with vars:"
+env|sort
+
+if [[ "$inner_script" == "deploy-bundle.sh" ]] ; then
+  if [[ "$DEPLOY_AS_HA_MODE" == "true" ]] ; then
+    echo "ERROR: bundle deployment doesn't support HA mode"
+    exit 1
+  fi
+  if [[ "$USE_ADDITIONAL_INTERFACE" == "true" ]] ; then
+    echo "ERROR: bundle deployment doesn't support deploying with additional interface"
+    exit 1
+  fi
+fi
+
+if ! juju-bootstrap ; then
+  echo "ERROR: Bootstrap error. exiting..."
+  exit 1
+fi
+
+# this code is only for juju 2.0
+iid=`juju show-controller amazon --format yaml | awk '/instance-id/{print $2}'`
+if [ -n "$iid" ] ; then
+  AZ=`aws ec2 describe-instances --instance-id "$iid" --query 'Reservations[*].Instances[*].Placement.AvailabilityZone' --output text`
+  vpc_id=`aws ec2 describe-instances --instance-id "$iid" --query 'Reservations[*].Instances[*].VpcId' --output text`
+fi
+echo "INFO: Availability zone of this deployment is $AZ, vpc is $vpc_id"
+export AZ
+export vpc_id
+
+trap 'catch_errors $LINENO' ERR EXIT
+
+function catch_errors() {
+  local exit_code=$?
+  echo "Line: $1  Error=$exit_code  Command: '$(eval echo $BASH_COMMAND)'"
+  trap - ERR EXIT
+
+  $my_dir/../save-logs.sh
+  if [ -f $my_dir/save-logs.sh ] ; then
+    $my_dir/save-logs.sh
+  fi
+
+  if [[ "$CLEAN_ENV" == 'always' ]] ; then
+    cleanup_environment
+  fi
+
+  exit $exit_code
+}
+
+echo "--------------------------------------------- Run deploy script: $inner_script"
+$my_dir/$inner_script $script_params
+
+$my_dir/../common/check-openstack.sh
+
+if [[ "$RUN_TEMPEST" == 'true' ]] ; then
+  $my_dir/run-tempest.sh
+fi
+
+$my_dir/../save-logs.sh
+if [ -f $my_dir/save-logs.sh ] ; then
+  $my_dir/save-logs.sh
+fi
+
+if [[ "$CLEAN_ENV" == 'always' || "$CLEAN_ENV" == 'on_success' ]] ; then
+  cleanup_environment
+fi
+
+trap - ERR EXIT
---
+++ juju/aws-contrail-R5/deploy-bundle.sh
@@ -0,0 +1,91 @@
+#!/bin/bash -e
+
+my_file="$(readlink -e "$0")"
+my_dir="$(dirname $my_file)"
+source "$my_dir/../common/functions"
+source "$my_dir/functions"
+
+BUNDLE="$my_dir/openstack-contrail-amazon.yaml"
+
+trap 'catch_errors_ce $LINENO' ERR EXIT
+function catch_errors_ce() {
+  local exit_code=$?
+  echo "Line: $1  Error=$exit_code  Command: '$(eval echo $BASH_COMMAND)'"
+  trap - ERR EXIT
+  exit $exit_code
+}
+
+# it also sets variables with names
+check_containers
+
+if [ "$DEPLOY_AS_HA_MODE" != 'false' ] ; then
+  echo "ERROR: Deploy from the bundle doesn't support HA mode."
+  exit 1
+fi
+
+jver="$(juju-version)"
+deploy_from=${1:-github}   # Place where to get charms - github or charmstore
+if [[ "$deploy_from" == github ]] ; then
+  if [[ "$jver" == 1 ]] ; then
+    exit 1
+  else
+    # version 2
+    JUJU_REPO="$WORKSPACE/contrail-charms"
+  fi
+else
+  # deploy_from=charmstore
+  echo "ERROR: Deploy from charmstore is not supported yet"
+  exit 1
+fi
+
+echo "---------------------------------------------------- From: $JUJU_REPO  Version: $VERSION"
+
+prepare_repo
+repo_ip=`get-machine-ip-by-number $mrepo`
+repo_key=`curl -s http://$repo_ip/ubuntu/repo.key`
+repo_key=`echo "$repo_key" | awk '{printf("          %s\r", $0)}'`
+
+# change bundles' variables
+echo "INFO: Change variables in bundle..."
+rm -f "$BUNDLE.tmp"
+cp "$BUNDLE" "$BUNDLE.tmp"
+BUNDLE="$BUNDLE.tmp"
+sed -i -e "s/%SERIES%/$SERIES/m" $BUNDLE
+sed -i -e "s/%OPENSTACK_ORIGIN%/$OPENSTACK_ORIGIN/m" $BUNDLE
+sed -i -e "s/%PASSWORD%/$PASSWORD/m" $BUNDLE
+sed -i -e "s|%JUJU_REPO%|$JUJU_REPO|m" $BUNDLE
+sed -i -e "s|%REPO_IP%|$repo_ip|m" $BUNDLE
+sed -i -e "s|%REPO_KEY%|$repo_key|m" $BUNDLE
+if [ "$USE_EXTERNAL_RABBITMQ" == 'true' ]; then
+  sed -i -e "s|%USE_EXTERNAL_RABBITMQ%|true|m" $BUNDLE
+else
+  sed -i -e "s|%USE_EXTERNAL_RABBITMQ%|false|m" $BUNDLE
+fi
+sed -i -e "s|%AUTH_MODE%|$AAA_MODE|m" $BUNDLE
+sed -i "s/\r/\n/g" $BUNDLE
+
+echo "INFO: Deploy bundle $(date)"
+juju-deploy-bundle $BUNDLE
+
+if [ "$USE_EXTERNAL_RABBITMQ" == 'true' ]; then
+  juju-add-relation "contrail-controller" "rabbitmq-server:amqp"
+fi
+
+echo "INFO: Detect machines $(date)"
+detect_machines
+cleanup_computes
+echo "INFO: Set endpoints $(date)"
+hack_openstack
+echo "INFO: Apply SSL flag if set $(date)"
+apply_ssl
+
+echo "INFO: Attach contrail-controller container $(date)"
+juju-attach contrail-controller contrail-controller="$HOME/docker/$image_controller"
+echo "INFO: Attach contrail-analyticsdb container $(date)"
+juju-attach contrail-analyticsdb contrail-analyticsdb="$HOME/docker/$image_analyticsdb"
+echo "INFO: Attach contrail-analytics container $(date)"
+juju-attach contrail-analytics contrail-analytics="$HOME/docker/$image_analytics"
+
+post_deploy
+
+trap - ERR EXIT
