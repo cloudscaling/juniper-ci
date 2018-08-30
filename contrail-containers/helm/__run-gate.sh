42d847c275a2073c546575f3b7a248402ffca958
Andrey Pavlov <andrey.mp@gmail.com>
2018-08-30 15:17:04 +0300

    containers/helm: enable dpdk

--- contrail-containers/helm/__run-gate.sh
+++ contrail-containers/helm/__run-gate.sh
@@ -216,6 +216,8 @@ global:
     CONFIG_DATABASE_NODEMGR__DEFAULTS__minimum_diskGB: "2"
     DATABASE_NODEMGR__DEFAULTS__minimum_diskGB: "2"
     VROUTER_GATEWAY: 10.$((NET_BASE_PREFIX+1)).$JOB_RND.1
+manifests:
+  configmap_vrouter_dpdk: true
 EOF
 
 helm install --name contrail-thirdparty ${CHD_PATH}/contrail-thirdparty --namespace=contrail --values=/tmp/contrail.yaml
