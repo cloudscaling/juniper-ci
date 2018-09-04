55e9b1cd0bbcce64ab48823be19f6994a1378023
Andrey Pavlov <andrey.mp@gmail.com>
2018-09-04 14:24:09 +0300

    containers: tune dpdk memory settings

--- contrail-containers/helm/__run-gate.sh
+++ contrail-containers/helm/__run-gate.sh
@@ -216,8 +216,7 @@ global:
     CONFIG_DATABASE_NODEMGR__DEFAULTS__minimum_diskGB: "2"
     DATABASE_NODEMGR__DEFAULTS__minimum_diskGB: "2"
     VROUTER_GATEWAY: 10.$((NET_BASE_PREFIX+1)).$JOB_RND.1
-    HUGE_PAGES: "1024"
-    DPDK_MEM_PER_SOCKET: "192"
+    HUGE_PAGES: "2048"
     VROUTER_ENCRYPTION: FALSE
 manifests:
   configmap_vrouter_dpdk: true
