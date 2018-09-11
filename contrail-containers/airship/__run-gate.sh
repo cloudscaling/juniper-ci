de96f0476d922e894719334d74fc7b5e76bb52db
Andrey Pavlov <andrey.mp@gmail.com>
2018-09-11 18:25:27 +0300

    containers/airship: change docker flags

--- contrail-containers/airship/__run-gate.sh
+++ contrail-containers/airship/__run-gate.sh
@@ -7,5 +7,10 @@ mkdir -p $my_dir/logs
 source "$my_dir/cloudrc"
 
 git clone https://github.com/openstack/airship-in-a-bottle
+git clone https://git.openstack.org/openstack/airship-pegleg.git
+git clone https://git.openstack.org/openstack/airship-shipyard.git
+
+sed -i 's/-it/-i/g' airship-pegleg/tools/pegleg.sh
+
 cd ./airship-in-a-bottle/manifests/dev_single_node
 ./airship-in-a-bottle.sh -y
