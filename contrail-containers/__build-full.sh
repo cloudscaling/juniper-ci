72639b063a3e158c8671c47e199b1940269032ec
Andrey Pavlov <andrey.mp@gmail.com>
2018-11-28 17:54:00 +0300

    containers: add hacks to build-full

--- contrail-containers/__build-full.sh
+++ contrail-containers/__build-full.sh
@@ -21,11 +21,26 @@ echo "INFO: Start build $(date)"
 
 git clone https://github.com/Juniper/contrail-dev-env.git
 cd contrail-dev-env
+
+# hack contrail-dev-env for our configuration/settings.
+# TODO: make all of them configurable
+default_interface=`ip route show | grep "default via" | awk '{print $5}'`
+registry_ip=`ip address show dev $default_interface | head -3 | tail -1 | tr "/" " " | awk '{print $2}'`
+sed -i -e "s/registry/${registry_ip}/g" common.env.tmpl
+sed -i -e "s/contrail-registry/${registry_ip}/g" vars.yaml.tmpl
+sed -i -e "s/registry/${registry_ip}/g" dev_config.yaml.tmpl
+sed -i -e "s/registry/${registry_ip}/g" daemon.json.tmpl
+sed -i -e "s/6666/5000/g" common.env.tmpl
+sed -i -e "s/6666/5000/g" vars.yaml.tmpl
+sed -i -e "s/6666/5000/g" daemon.json.tmpl
+sed -i -e "s/6666/5000/g" startup.sh
+
 sudo ./startup.sh
 docker ps -a
 
 cat >build.sh <<EOF
 #!/bin/bash -ex
+export OPENSTACK_VERSION=$OPENSTACK_VERSION
 cd /root/contrail-dev-env
 make sync
 make fetch_packages
