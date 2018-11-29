601dfc77f6333f78f5ec87cf983b3a6bc2918502
Matthew Mosesohn <matthew.mosesohn@gmail.com>
2018-11-29 14:46:01 +0300

    precreate /etc/novajoin/join.conf

--- tripleo/__undercloud-install-1-as-root.sh
+++ tripleo/__undercloud-install-1-as-root.sh
@@ -245,6 +245,8 @@ if [[ "$FREE_IPA" == 'true' ]] ; then
   # Pinned versions to avoid conflict with system PyYAML package
   pip install novajoin==1.0.21 oslo.policy==1.33.2
   yum install -y ipa-client
+  mkdir -p /etc/novajoin
+  touch /etc/novajoin/join.conf
   env_opts+=" FREE_IPA_OTP=$FREE_IPA_OTP"
 fi
 
