9a105b8a154592b130fc5e8a2dd65f0f3d74f6de
Andrey Pavlov <andrey.mp@gmail.com>
2018-12-21 16:05:13 +0300

    containers: remove old check for net_count

--- contrail-containers/common/aws/create-vm.sh
+++ contrail-containers/common/aws/create-vm.sh
@@ -3,12 +3,7 @@
 my_file="$(readlink -e "$0")"
 my_dir="$(dirname $my_file)"
 
-NET_COUNT=${NET_COUNT:-2}
-if (( NET_COUNT > 2 )) ; then
-  echo "NET_COUNT more than 2 is not supported."
-  exit 1
-fi
-
+NET_COUNT=${NET_COUNT:-1}
 SSH_OPTS="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ServerAliveInterval=30"
 ENV_FILE="$WORKSPACE/cloudrc"
 
