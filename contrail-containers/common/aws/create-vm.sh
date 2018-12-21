6e8dd0e05c4062ec563789523cc5a714fb591c7d
Andrey Pavlov <andrey.mp@gmail.com>
2018-12-21 16:28:40 +0300

    containers/aws: use real iface index for iface attach

--- contrail-containers/common/aws/create-vm.sh
+++ contrail-containers/common/aws/create-vm.sh
@@ -135,7 +135,7 @@ function run_instance() {
     for ((i=1; i<NET_COUNT; ++i)); do
       sid=${subnet_ids[i-1]}
       eni_id=`aws ${AWS_FLAGS} ec2 create-network-interface --subnet-id $sid --query 'NetworkInterface.NetworkInterfaceId' --output text`
-      eni_attach_id=`aws ${AWS_FLAGS} ec2 attach-network-interface --network-interface-id $eni_id --instance-id $instance_id --device-index 1 --query 'AttachmentId' --output text`
+      eni_attach_id=`aws ${AWS_FLAGS} ec2 attach-network-interface --network-interface-id $eni_id --instance-id $instance_id --device-index $i --query 'AttachmentId' --output text`
       aws ${AWS_FLAGS} ec2 modify-network-interface-attribute --network-interface-id $eni_id --attachment AttachmentId=$eni_attach_id,DeleteOnTermination=true
       echo "INFO: additional interface $eni_id is attached: $eni_attach_id"
     done
