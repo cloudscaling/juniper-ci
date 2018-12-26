c55c55dad8f1f0004041e374cb2f081d9b2542cb
Alexey Osorgin <alexey.osorgin@gmail.com>
2018-12-26 16:11:59 +0300

    Add list-environment job

---
+++ contrail-containers/common/server/list-env.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+my_file="$(readlink -e "$0")"
+my_dir="$(dirname $my_file)"
+
+existed_vms=`virsh -q list --all | awk '{print $2}' | sort`
+
+echo "Virtual machines:"
+for vm in $existed_vms ; do
+    echo $vm
+done
+
+echo
+echo "Virtual machines\' dumps:""
+for vm in $existed_vms ; do
+    echo "Name: $vm"
+    echo "Dump:"
+    vm_dump=`virsh dumpxml $vm`
+    echo "$vm_dump"
+    echo
+done
