2a9ea44479d913b157135fb334bb258b6c2f0d36
Andrey Pavlov <andrey.mp@gmail.com>
2018-12-26 18:11:44 +0300

    containers: remove anone job

--- contrail-containers/anone/__run-gate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -ex
-
-mkdir -p /root/.ssh && cp /.ssh/* /root/.ssh/ && chown root:root /root/.ssh
-cd /root
-
-cd contrail-ansible-deployer
-ansible-playbook -v -e orchestrator=none -e config_file=/root/contrail-ansible-deployer/instances.yaml playbooks/configure_instances.yml
-ansible-playbook -v -e orchestrator=none -e config_file=/root/contrail-ansible-deployer/instances.yaml playbooks/install_contrail.yml
