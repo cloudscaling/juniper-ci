18234e7463ab314224b8bedefd34a19b0d4b9dc7
Andrey Pavlov <andrey.mp@gmail.com>
2019-01-16 15:15:51 +0300

    containers/airship: try with upstream images

--- contrail-containers/airship/__run-gate.sh
+++ contrail-containers/airship/__run-gate.sh
@@ -73,7 +73,7 @@ if [[ -n "$domain" ]] ; then
   sed -i "s/cluster_domain: cluster.local/cluster_domain: $domain/" $COMMON_CONFIG_FILE
 fi
 
-export PEGLEG_IMAGE="quay.io/airshipit/pegleg:1ada48cc360ec52c7ab28b96c28a0c7df8bcee40"
-export PROMENADE_IMAGE="quay.io/airshipit/promenade:77073ddd6f1a445deae741afe53d858ba39f0e76"
+#export PEGLEG_IMAGE="quay.io/airshipit/pegleg:1ada48cc360ec52c7ab28b96c28a0c7df8bcee40"
+#export PROMENADE_IMAGE="quay.io/airshipit/promenade:77073ddd6f1a445deae741afe53d858ba39f0e76"
 
 ../common/deploy-airship.sh demo
