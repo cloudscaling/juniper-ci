1acbb07c807f1898044bcfcc579aac1989ee3912
Andrey Pavlov <andrey.mp@gmail.com>
2019-02-21 11:41:52 +0300

    containers: add clean vm deployment

---
+++ contrail-containers/vm/run.sh
@@ -0,0 +1,56 @@
+#!/bin/bash -eEa
+
+my_file="$(readlink -e "$0")"
+my_dir="$(dirname $my_file)"
+
+if [[ "$CLEAN_BEFORE" == 'true' || "$CLEAN_BEFORE" == 'clean_and_exit' ]] ; then
+  $my_dir/../common/${HOST}/cleanup.sh || /bin/true
+  if [[ "$CLEAN_BEFORE" == 'clean_and_exit' ]] ; then
+    exit
+  fi
+fi
+
+rm -rf "$WORKSPACE/logs"
+mkdir -p "$WORKSPACE/logs"
+
+# definition for job deployment
+source $my_dir/${HOST}-defs
+source $my_dir/../common/functions
+source $my_dir/../common/check-functions
+
+$my_dir/../common/${HOST}/create-vm.sh
+source "$my_dir/../common/${HOST}/setup-defs"
+
+trap 'catch_errors $LINENO' ERR
+function catch_errors() {
+  local exit_code=$?
+  echo "Line: $1  Error=$exit_code  Command: '$(eval echo $BASH_COMMAND)'"
+
+  save_logs '1-'
+  if [[ "$CLEAN_ENV" == 'always' ]] ; then
+    $my_dir/../common/${HOST}/cleanup.sh
+  fi
+
+  exit $exit_code
+}
+
+if [[ "$CONTAINER_REGISTRY" == 'build' || "$CONTAINER_REGISTRY" == 'fullbuild' ]]; then
+  source "$my_dir/../common/${HOST}/definitions"
+  build_containers
+  CONTAINER_REGISTRY="$build_ip:5000"
+  CONTRAIL_VERSION="$OPENSTACK_VERSION-$CONTRAIL_VERSION"
+fi
+
+# deploy cloud
+source "$my_dir/../common/${HOST}/${ENVIRONMENT_OS}"
+
+# stay clean&ready for user
+
+# save logs and exit
+trap - ERR
+save_logs '1-'
+if [[ "$CLEAN_ENV" == 'always' || "$CLEAN_ENV" == 'on_success' ]] ; then
+  $my_dir/../common/${HOST}/cleanup.sh
+fi
+
+exit $res
