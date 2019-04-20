90afd1a582a773c1eeaa3a04f76b708adb2559c5
Andrey Pavlov <andrey.mp@gmail.com>
2019-04-20 21:22:09 +0300

    devstack: change flavor

--- devstack/create-instance-for-devstack-cloud.sh
+++ devstack/create-instance-for-devstack-cloud.sh
@@ -2,7 +2,7 @@
 
 ENV_FILE="cloudrc"
 VM_CIDR="192.168.130.0/24"
-VM_TYPE="r3.xlarge"
+VM_TYPE="r4.xlarge"
 # ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-20150325
 # us-east-1 IMAGE_ID="ami-d05e75b8"
 # ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-201704
