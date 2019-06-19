54c83704d6f460b7c85d8e56def713b80c5c05dc
OlegBravo <50629642+OlegBravo@users.noreply.github.com>
2019-06-19 19:27:52 +0300

    migrate to treasuremap

--- contrail-containers/airship/__run-gate.sh
+++ contrail-containers/airship/__run-gate.sh
@@ -65,7 +65,7 @@ export HOSTIP=$LOCAL_IP
 # x/32 will work for CEPH in a single node deploy.
 export HOSTCIDR=$LOCAL_IP/32
 
-COMMON_CONFIG_FILE="../../site/$TARGET_SITE/networks/common-addresses.yaml"
+COMMON_CONFIG_FILE="../../../site/$TARGET_SITE/networks/common-addresses.yaml "
 if grep -q "10.96.0.10" "/etc/resolv.conf"; then
   echo "INFO: Not changing DNS servers, /etc/resolv.conf already updated."
 else
