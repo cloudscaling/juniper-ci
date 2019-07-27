e53a357f37f80bbf749166f159ff594e5a559242
Andrey Pavlov <andrey.mp@gmail.com>
2019-07-27 22:37:47 +0300

    juju: source k8s functions

--- juju/aws-contrail-R5-k8s/deploy-all.sh
+++ juju/aws-contrail-R5-k8s/deploy-all.sh
@@ -9,6 +9,7 @@ fi
 my_file="$(readlink -e "$0")"
 my_dir="$(dirname $my_file)"
 source "$my_dir/../common/functions"
+source "$my_dir/functions"
 
 if [[ "$CLEAN_BEFORE" == 'true' || "$CLEAN_BEFORE" == 'clean_and_exit' ]] ; then
   cleanup_environment
