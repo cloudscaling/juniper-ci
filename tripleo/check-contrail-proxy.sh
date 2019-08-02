f9bf89fb3fb06c14be9d6671860b57d8f8b69f3b
alexey-mr <alexey.morlang@gmail.com>
2019-08-02 17:59:02 +0300

    [tripelo] keep VMs after check openstack

--- tripleo/check-contrail-proxy.sh
+++ tripleo/check-contrail-proxy.sh
@@ -13,6 +13,7 @@ ssh_opts=$@
 
 MAX_FAIL=${MAX_FAIL:-30}
 SSH_CMD=${SSH_CMD:-'ssh'}
+KEEP_VM_AFTER_TEST=${KEEP_VM_AFTER_TEST:-"true"}
 
 cos_dir='/home/stack/check-openstack'
 ssh -T $ssh_opts $ssh_addr "mkdir -p $cos_dir/tripleo"
@@ -22,4 +23,5 @@ ssh -T $ssh_opts $ssh_addr "chown -R stack:stack $cos_dir"
 run_opts="SSH_CMD=$SSH_CMD MAX_FAIL=$MAX_FAIL DEBUG=$DEBUG OPENSTACK_VERSION=$OPENSTACK_VERSION"
 run_opts+=" TLS=$TLS DPDK=$DPDK TSN=$TSN KEYSTONE_API_VERSION=$KEYSTONE_API_VERSION"
 run_opts+=" FREE_IPA=$FREE_IPA"
+run_opts+=" KEEP_VM_AFTER_TEST=$KEEP_VM_AFTER_TEST"
 ssh -T $ssh_opts $ssh_addr "sudo -u stack $run_opts $cos_dir/tripleo/check-openstack.sh"
