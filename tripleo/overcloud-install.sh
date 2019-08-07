e329daec1434e1048dcda9cae04839f9dbe00d5a
alexey-mr <alexey.morlang@gmail.com>
2019-08-07 15:08:22 +0300

    [tripleo] add sourcing of functions into overcloud install

--- tripleo/overcloud-install.sh
+++ tripleo/overcloud-install.sh
@@ -3,6 +3,8 @@
 my_file="$(readlink -e "$0")"
 my_dir="$(dirname $my_file)"
 
+source "$my_dir/functions"
+
 if [[ -z "$NUM" ]] ; then
   echo "Please set NUM variable to specific environment wber. (export NUM=4)"
   exit 1
