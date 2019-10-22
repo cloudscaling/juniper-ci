edc156e4a2b10bb0940c450774da9e98fef34dee
Andrey Pavlov <andrey-mp@yandex.ru>
2019-10-22 21:39:24 +0300

    juju-r4: try another escaping for kernel operations

--- juju/bmc-contrail-R4/deploy_manual.sh
+++ juju/bmc-contrail-R4/deploy_manual.sh
@@ -48,9 +48,9 @@ fi
 
 set -x
 juju-ssh $comp1 "sudo DEBIAN_FRONTEND=noninteractive apt-get install -fy linux-image-4.4.0-116-generic linux-headers-4.4.0-116-generic"
-juju-ssh $comp1 "sudo sed -i \"s/\$(uname -r)/4.4.0-116-generic/g\" /boot/grub/grub.cfg ; sudo reboot" || /bin/true
+juju-ssh $comp1 'sudo sed -i "s/$(uname -r)/4.4.0-116-generic/g" /boot/grub/grub.cfg ; sudo reboot' || /bin/true
 juju-ssh $comp2 "sudo DEBIAN_FRONTEND=noninteractive apt-get install -fy linux-image-4.4.0-116-generic linux-headers-4.4.0-116-generic"
-juju-ssh $comp2 "sudo sed -i \"s/\$(uname -r)/4.4.0-116-generic/g\" /boot/grub/grub.cfg ; sudo reboot" || /bin/true
+juju-ssh $comp2 'sudo sed -i "s/$(uname -r)/4.4.0-116-generic/g" /boot/grub/grub.cfg ; sudo reboot' || /bin/true
 set +x
 
 # it also sets variables with names
