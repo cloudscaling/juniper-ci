90ac1390c22d903864b74922505dd9efb566c2db
Andrey Pavlov <andrey-mp@yandex.ru>
2020-01-19 13:49:05 +0300

    update devstack scripts

--- devstack/install-devstack.sh
+++ devstack/install-devstack.sh
@@ -24,36 +24,38 @@ while [ $($SSH 'pwd > /dev/null' ; echo $?) != 0 ]; do
 done
 
 echo -------------------------------------------------------------------------- $(date)
-$SSH "(echo o; echo n; echo p; echo 1; echo ; echo ; echo w) | sudo fdisk /dev/xvdh"
-$SSH "sudo mkfs.ext4 /dev/xvdh1"
-$SSH "sudo mkdir -p /opt/stack"
-$SSH "sudo su -c \"echo '/dev/xvdh1  /opt/stack  auto  defaults,auto  0  0' >> /etc/fstab\""
-$SSH "sudo mount /opt/stack"
-$SSH "sudo chown \$USER /opt/stack"
-$SSH "cd /opt/stack; git clone https://github.com/openstack/ec2api-tempest-plugin"
+cat <<EOF | $SSH
+(echo o; echo n; echo p; echo 1; echo ; echo ; echo w) | sudo fdisk /dev/xvdh
+sudo mkfs.ext4 /dev/xvdh1
+sudo mkdir -p /opt/stack
+sudo su -c \"echo '/dev/xvdh1  /opt/stack  auto  defaults,auto  0  0' >> /etc/fstab\"
+sudo mount /opt/stack
+sudo chown \$USER /opt/stack
+cd /opt/stack; git clone https://github.com/openstack/ec2api-tempest-plugin
 
-$SSH "sudo sed -i 's/# deb/deb/g' /etc/apt/sources.list"
-$SSH "sudo apt-get -qq update"
-$SSH "sudo DEBIAN_FRONTEND=noninteractive apt-get -fqy install git ebtables bridge-utils"
-$SSH "git clone https://github.com/openstack-dev/devstack.git"
+sudo sed -i 's/# deb/deb/g' /etc/apt/sources.list
+sudo apt-get -qq update
+sudo DEBIAN_FRONTEND=noninteractive apt-get -fqy install git ebtables bridge-utils
+git clone https://github.com/openstack-dev/devstack.git
 
-$SSH "sudo mkdir /var/log/journal"
-$SSH "sudo mkdir /etc/systemd/journald.conf.d"
-$SSH "echo [Journal] | sudo tee /etc/systemd/journald.conf.d/size.conf > /dev/null"
-$SSH "echo SystemMaxUse=1G | sudo tee -a /etc/systemd/journald.conf.d/size.conf > /dev/null"
-$SSH "echo RuntimeMaxUse=1G | sudo tee -a /etc/systemd/journald.conf.d/size.conf > /dev/null"
-$SSH "echo Storage=persistent | sudo tee -a /etc/systemd/journald.conf.d/size.conf > /dev/null"
-$SSH "sudo systemctl restart systemd-journald || true"
+sudo mkdir /var/log/journal
+sudo mkdir /etc/systemd/journald.conf.d
+echo [Journal] | sudo tee /etc/systemd/journald.conf.d/size.conf > /dev/null
+echo SystemMaxUse=1G | sudo tee -a /etc/systemd/journald.conf.d/size.conf > /dev/null
+echo RuntimeMaxUse=1G | sudo tee -a /etc/systemd/journald.conf.d/size.conf > /dev/null
+echo Storage=persistent | sudo tee -a /etc/systemd/journald.conf.d/size.conf > /dev/null
+sudo systemctl restart systemd-journald || true
+EOF
 
 echo -------------------------------------------------------------------------- $(date)
 cp $localrcfile localrc
 sed -i "s\^SERVICE_HOST.*$\SERVICE_HOST=$public_ip\m" localrc
 $SCP localrc $SSH_DEST:devstack/localrc
 echo "Installing devstack"
-$SSH "cd devstack; ./stack.sh < /dev/null > stack.log 2>&1"
+$SSH "cd devstack; ./stack.sh < /dev/null" &> stack.log
 exit_code=$?
 if [[ $exit_code != 0 ]]; then
-  $SSH "cat devstack/stack.log"
+  cat stack.log
   exit $exit_code
 fi
 echo -------------------------------------------------------------------------- $(date)
